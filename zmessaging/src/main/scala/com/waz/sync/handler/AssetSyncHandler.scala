d77725cde812cf6f09bb8201d5b9ed3f6935344c
Maciej Gorywoda <makingthematrix@gmail.com>
2018-08-02 13:44:21 +0200

    Revert "Revert "Bump upload file size limit to 100MB for teams""
    This reverts commit 0e84ca250e8a7b321022d14a6599fe928d2ec92e.

--- zmessaging/src/main/scala/com/waz/sync/handler/AssetSyncHandler.scala
+++ zmessaging/src/main/scala/com/waz/sync/handler/AssetSyncHandler.scala
@@ -29,7 +29,8 @@ import com.waz.sync.otr.OtrSyncHandler
 import com.waz.threading.{CancellableFuture, Threading}
 import com.waz.sync.client.ErrorOrResponse
 
-class AssetSyncHandler(cache:   CacheService,
+class AssetSyncHandler(teamId:  Option[TeamId],
+                       cache:   CacheService,
                        assets:  AssetService,
                        otrSync: OtrSyncHandler) {
 
@@ -37,8 +38,8 @@ class AssetSyncHandler(cache:   CacheService,
 
   def uploadAssetData(assetId: AssetId, public: Boolean = false, retention: Retention): ErrorOrResponse[AssetData] =
     CancellableFuture.lift(assets.updateAsset(assetId, asset => asset.copy(status = if (asset.status == UploadNotStarted) UploadInProgress else asset.status )).zip(assets.getLocalData(assetId))) flatMap {
-      case (Some(asset), Some(data)) if data.length > AssetData.MaxAllowedAssetSizeInBytes =>
-        debug(s"Local data too big. Data length: ${data.length}, max size: ${AssetData.MaxAllowedAssetSizeInBytes}, local data: $data, asset: $asset")
+      case (Some(asset), Some(data)) if data.length > AssetData.maxAssetSizeInBytes(teamId.isDefined) =>
+        debug(s"Local data too big. Data length: ${data.length}, max size: ${AssetData.maxAssetSizeInBytes(teamId.isDefined)}, local data: $data, asset: $asset")
         CancellableFuture successful Left(internalError(AssetSyncHandler.AssetTooLarge))
 
       case (Some(asset), _) if asset.remoteId.isDefined =>
