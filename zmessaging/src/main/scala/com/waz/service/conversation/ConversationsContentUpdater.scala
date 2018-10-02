wireapp/wire-android-sync-engine

fix: removes duplicated entries in the conversations table
    A bug introduced in a previous release broke the way new conversations were created. Instead of adding one entry to
    the conversations table and then updating it, the bug was creating a new entry, with a new local id, and the same remote id.
    The app wasn't then able to display the new conversation on the conversations list, and display messages in this conversation.
    The bug was fixed, but the duplicate entries already created were left in the database. This commit removes them
    and corrects the original one. It should run only once, just after the update to the release with the fix.

    We can suspect that in at most a few months all clients with the duplicated entries in the database will be fixed and
    we will be able to remove the code.

    fixes: https://github.com/wireapp/android-project/issues/255
    fixes: https://github.com/wireapp/android-project/issues/245

zmessaging/src/main/scala/com/waz/service/conversation/ConversationsContentUpdater.scala
@@ -65,6 +65,7 @@ class ConversationsContentUpdaterImpl(val storage:     ConversationStorage,
                                       selfUserId:      UserId,
                                       teamId:          Option[TeamId],
                                       usersStorage:    UsersStorage,
+                                      userPrefs:       UserPreferences,
                                       membersStorage:  MembersStorage,
                                       messagesStorage: => MessagesStorage,
                                       tracking:        TrackingService) extends ConversationsContentUpdater {
@@ -82,6 +83,14 @@ class ConversationsContentUpdaterImpl(val storage:     ConversationStorage,
       conv.cleared.foreach(messagesStorage.clear(conv.id, _).recoverWithLog())
     }
   }
+
+  private val shouldFixDuplicatedConversations = userPrefs.preference(UserPreferences.FixDuplicatedConversations)
+
+  for {
+    shouldFix <- shouldFixDuplicatedConversations()
+    _         <- if (shouldFix) fixDuplicatedConversations() else Future.successful({})
+    _         <- if (shouldFix) shouldFixDuplicatedConversations := false else Future.successful({})
+  } yield {}
 
   override def convById(id: ConvId): Future[Option[ConversationData]] = storage.get(id)
 
@@ -213,4 +222,42 @@ class ConversationsContentUpdaterImpl(val storage:     ConversationStorage,
 
   override def updateAccessMode(id: ConvId, access: Set[Access], accessRole: Option[AccessRole], link: Option[ConversationData.Link] = None) =
     storage.update(id, conv => conv.copy(access = access, accessRole = accessRole, link = if (!access.contains(Access.CODE)) None else link.orElse(conv.link)))
+
+  def fixDuplicatedConversations(): Future[Unit] = {
+
+    def moveMessages(from: ConvId, to: ConvId): Future[Unit] =
+      for {
+        messages <- messagesStorage.findLocalFrom(from, RemoteInstant.Epoch)
+        _        <- messagesStorage.updateAll2(messages.map(_.id), { m => m.copy(convId = to) })
+      } yield ()
+
+    for {
+      convs      <- storage.list()
+      duplicates =  convs.groupBy(_.remoteId).filter(_._2.size > 1).map(_._2.map(_.id))
+      convIds    =  duplicates.flatten.toSet
+      users      <- usersStorage.listAll(convIds.map(id => UserId(id.str)))
+      userIdSet  =  users.map(_.id).toSet
+      isOriginal =  convIds.map(id => id -> userIdSet.contains(UserId(id.str))).toMap
+      pairs      =  duplicates.flatMap { pair =>
+        val (original, updates) = pair.partition(isOriginal)
+        (original.headOption, updates) match {
+          // there should be exactly one original and one or more duplicates
+          case (Some(orig), upd) if upd.nonEmpty => Some(orig, upd)
+          case _ => None
+        }
+      }.toMap
+      _ = verbose(s"fixDuplicatedConversations: (original, updates) pairs: $pairs")
+      _ <- Future.sequence(pairs.map { case (origId, updates) =>
+        for {
+          _ <- Future.sequence(updates.map(updId => moveMessages(updId, origId)))
+          _ <- if (updates.tail.nonEmpty) storage.removeAll(updates.tail) else Future.successful({})
+          _ <- storage.updateLocalId(updates.head, origId)
+          _ <- Future.sequence(updates.map(membersStorage.delete))
+          _ <- updateConversation(origId, Some(ConversationType.OneToOne), hidden = Some(false))
+          _ <- membersStorage.add(origId, Seq(selfUserId, UserId(origId.str)))
+        } yield ()
+      })
+    } yield storage.refreshRemoteMap()
+  }
+
 }

wireapp/wire-android_legacy

zmessaging/src/main/scala/com/waz/service/conversation/ConversationsContentUpdater.scala
@@ -65,6 +65,7 @@ class ConversationsContentUpdaterImpl(val storage:     ConversationStorage,
                                       selfUserId:      UserId,
                                       teamId:          Option[TeamId],
                                       usersStorage:    UsersStorage,
+                                      userPrefs:       UserPreferences,
                                       membersStorage:  MembersStorage,
                                       messagesStorage: => MessagesStorage,
                                       tracking:        TrackingService) extends ConversationsContentUpdater {
@@ -82,6 +83,14 @@ class ConversationsContentUpdaterImpl(val storage:     ConversationStorage,
       conv.cleared.foreach(messagesStorage.clear(conv.id, _).recoverWithLog())
     }
   }
+
+  private val shouldFixDuplicatedConversations = userPrefs.preference(UserPreferences.FixDuplicatedConversations)
+
+  for {
+    shouldFix <- shouldFixDuplicatedConversations()
+    _         <- if (shouldFix) fixDuplicatedConversations() else Future.successful({})
+    _         <- if (shouldFix) shouldFixDuplicatedConversations := false else Future.successful({})
+  } yield {}
 
   override def convById(id: ConvId): Future[Option[ConversationData]] = storage.get(id)
 
@@ -214,4 +223,41 @@ class ConversationsContentUpdaterImpl(val storage:     ConversationStorage,
   override def updateAccessMode(id: ConvId, access: Set[Access], accessRole: Option[AccessRole], link: Option[ConversationData.Link] = None) =
     storage.update(id, conv => conv.copy(access = access, accessRole = accessRole, link = if (!access.contains(Access.CODE)) None else link.orElse(conv.link)))
 
+
+  def fixDuplicatedConversations(): Future[Unit] = {
+
+    def moveMessages(from: ConvId, to: ConvId): Future[Unit] =
+      for {
+        messages <- messagesStorage.findMessagesFrom(from, RemoteInstant.Epoch)
+        _        <- messagesStorage.updateAll2(messages.map(_.id), { m => m.copy(convId = to) })
+      } yield ()
+
+    for {
+      convs      <- storage.list()
+      duplicates =  convs.groupBy(_.remoteId).filter(_._2.size > 1).map(_._2.map(_.id))
+      convIds    =  duplicates.flatten.toSet
+      users      <- usersStorage.listAll(convIds.map(id => UserId(id.str)))
+      userIdSet  =  users.map(_.id).toSet
+      isOriginal =  convIds.map(id => id -> userIdSet.contains(UserId(id.str))).toMap
+      pairs      =  duplicates.flatMap { pair =>
+        val (original, updates) = pair.partition(isOriginal)
+        (original.headOption, updates) match {
+          // there should be exactly one original and one or more duplicates
+          case (Some(orig), upd) if upd.nonEmpty => Some(orig, upd)
+          case _ => None
+        }
+      }.toMap
+      _ = verbose(s"fixDuplicatedConversations: (original, updates) pairs: $pairs")
+      _ <- Future.sequence(pairs.map { case (origId, updates) =>
+        for {
+          _ <- Future.sequence(updates.map(updId => moveMessages(updId, origId)))
+          _ <- if (updates.tail.nonEmpty) storage.removeAll(updates.tail) else Future.successful({})
+          _ <- storage.updateLocalId(updates.head, origId)
+          _ <- Future.sequence(updates.map(membersStorage.delete))
+          _ <- updateConversation(origId, Some(ConversationType.OneToOne), hidden = Some(false))
+          _ <- membersStorage.add(origId, Seq(selfUserId, UserId(origId.str)))
+        } yield ()
+      })
+    } yield storage.refreshRemoteMap()
+  }
 }