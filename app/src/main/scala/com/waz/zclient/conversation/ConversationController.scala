jpotier/wire-android

feat: Move mute notification settings to conv options
fixes https://github.com/wireapp/android-project/issues/301

    In case of group conversations the notifications options (all allowed / mentions only / all muted)
    should appear on the group participants view, and they should work just as "Timed messages" options.
    The entry from the bottom popup menu should be removed.
    In case of one-to-one conversations the notifications options should stay in the bottom popup menu.

    Note: We are waiting for the WireStyleKit notification icon from the Design. Right now I use
    the "Mute" icon instead. The code can be reviewed and tested as it is.

app/src/main/scala/com/waz/zclient/conversation/ConversationController.scala
@@ -321,4 +321,11 @@ object ConversationController {
     }
   }
 
+  lazy val MuteSets = Seq(MuteSet.AllAllowed, MuteSet.OnlyMentionsAllowed, MuteSet.AllMuted)
+
+  def muteSetDisplayStringId(muteSet: MuteSet): Int = muteSet match {
+    case MuteSet.AllMuted            => R.string.conversation__action__notifications_nothing
+    case MuteSet.OnlyMentionsAllowed => R.string.conversation__action__notifications_mentions_only
+    case _                           => R.string.conversation__action__notifications_everything
+  }
 }

app/src/main/scala/com/waz/zclient/participants/NotificationsOptionsMenuController.scala
@@ -18,7 +18,6 @@
 package com.waz.zclient.participants
 import android.content.Context
 import com.waz.model.{ConvId, MuteSet}
-import com.waz.service.ZMessaging
 import com.waz.utils.events.{EventContext, EventStream, Signal, SourceStream}
 import com.waz.zclient.conversation.ConversationController
 import com.waz.zclient.{Injectable, Injector, R}
@@ -26,32 +25,38 @@ import com.waz.zclient.participants.OptionsMenuController.MenuItem
 import com.waz.zclient.utils.ContextUtils.getString
 
 class NotificationsOptionsMenuController(convId: ConvId, fromConversationList: Boolean)(implicit injector: Injector, context: Context, ec: EventContext) extends OptionsMenuController with Injectable {
+  import NotificationsOptionsMenuController._
 
-  private val zms = inject[Signal[ZMessaging]]
-  private val conversation = zms.flatMap(_.convsStorage.signal(convId))
   private val convController = inject[ConversationController]
+  private val conversation = convController.conversationData(convId)
 
   override val title: Signal[Option[String]] =
     if (fromConversationList)
-      conversation.map(_.displayName).map(Some(_))
+      conversation.map(_.map(_.displayName))
     else
       Signal.const(Some(getString(R.string.conversation__action__notifications_title)))
   override val optionItems: Signal[Seq[OptionsMenuController.MenuItem]] = Signal.const(Seq(Everything, OnlyMentions, Nothing))
   override val onMenuItemClicked: SourceStream[OptionsMenuController.MenuItem] = EventStream()
-  override val selectedItems: Signal[Set[OptionsMenuController.MenuItem]] = conversation.map(_.muted).map {
-    case MuteSet.AllMuted => Set(Nothing)
-    case MuteSet.OnlyMentionsAllowed => Set(OnlyMentions)
-    case _ => Set(Everything)
-  }
+  override val selectedItems: Signal[Set[OptionsMenuController.MenuItem]] =
+    conversation.collect {
+      case Some(c) => Set(menuItem(c.muted))
+    }
 
   onMenuItemClicked.map {
-    case Everything => MuteSet.AllAllowed
+    case Everything   => MuteSet.AllAllowed
     case OnlyMentions => MuteSet.OnlyMentionsAllowed
-    case _ => MuteSet.AllMuted
+    case _            => MuteSet.AllMuted
   }.onUi(m => convController.setMuted(convId, muted = m))
 }
 
-object Everything   extends MenuItem(R.string.conversation__action__notifications_everything, None)
-object OnlyMentions extends MenuItem(R.string.conversation__action__notifications_mentions_only, None)
-object Nothing      extends MenuItem(R.string.conversation__action__notifications_nothing, None)
+object NotificationsOptionsMenuController {
+  object Everything   extends MenuItem(R.string.conversation__action__notifications_everything, None)
+  object OnlyMentions extends MenuItem(R.string.conversation__action__notifications_mentions_only, None)
+  object Nothing      extends MenuItem(R.string.conversation__action__notifications_nothing, None)
 
+  def menuItem(muteSet: MuteSet): MenuItem = muteSet match {
+    case MuteSet.AllMuted            => Nothing
+    case MuteSet.OnlyMentionsAllowed => OnlyMentions
+    case _                           => Everything
+  }
+}

wireapp/wire-android_legacy

    In the group participants view I changed already "Notifications" to "Notifications and Replies",
    and moved the value text below the name text, as the design doc for quotes requires.
    I didn't change the variable names in the code, as that would create unnecessary conflicts with other
    PRs of the quotes feature.

app/src/main/scala/com/waz/zclient/conversation/ConversationController.scala
@@ -324,4 +324,11 @@ object ConversationController {
     }
   }
 
+  lazy val MuteSets = Seq(MuteSet.AllAllowed, MuteSet.OnlyMentionsAllowed, MuteSet.AllMuted)
+
+  def muteSetDisplayStringId(muteSet: MuteSet): Int = muteSet match {
+    case MuteSet.AllMuted            => R.string.conversation__action__notifications_nothing
+    case MuteSet.OnlyMentionsAllowed => R.string.conversation__action__notifications_mentions_and_replies
+    case _                           => R.string.conversation__action__notifications_everything
+  }
 }

app/src/main/scala/com/waz/zclient/participants/NotificationsOptionsMenuController.scala
@@ -18,7 +18,6 @@
 package com.waz.zclient.participants
 import android.content.Context
 import com.waz.model.{ConvId, MuteSet}
-import com.waz.service.ZMessaging
 import com.waz.utils.events.{EventContext, EventStream, Signal, SourceStream}
 import com.waz.zclient.conversation.ConversationController
 import com.waz.zclient.{Injectable, Injector, R}
@@ -26,32 +25,38 @@ import com.waz.zclient.participants.OptionsMenuController.BaseMenuItem
 import com.waz.zclient.utils.ContextUtils.getString
 
 class NotificationsOptionsMenuController(convId: ConvId, fromConversationList: Boolean)(implicit injector: Injector, context: Context, ec: EventContext) extends OptionsMenuController with Injectable {
+  import NotificationsOptionsMenuController._
 
-  private val zms = inject[Signal[ZMessaging]]
-  private val conversation = zms.flatMap(_.convsStorage.signal(convId))
   private val convController = inject[ConversationController]
+  private val conversation = convController.conversationData(convId)
 
   override val title: Signal[Option[String]] =
     if (fromConversationList)
-      conversation.map(_.displayName).map(Some(_))
+      conversation.map(_.map(_.displayName))
     else
       Signal.const(Some(getString(R.string.conversation__action__notifications_title)))
   override val optionItems: Signal[Seq[OptionsMenuController.MenuItem]] = Signal.const(Seq(Everything, OnlyMentions, Nothing))
   override val onMenuItemClicked: SourceStream[OptionsMenuController.MenuItem] = EventStream()
-  override val selectedItems: Signal[Set[OptionsMenuController.MenuItem]] = conversation.map(_.muted).map {
-    case MuteSet.AllMuted => Set(Nothing)
-    case MuteSet.OnlyMentionsAllowed => Set(OnlyMentions)
-    case _ => Set(Everything)
-  }
+  override val selectedItems: Signal[Set[OptionsMenuController.MenuItem]] =
+    conversation.collect {
+      case Some(c) => Set(menuItem(c.muted))
+    }
 
   onMenuItemClicked.map {
-    case Everything => MuteSet.AllAllowed
+    case Everything   => MuteSet.AllAllowed
     case OnlyMentions => MuteSet.OnlyMentionsAllowed
-    case _ => MuteSet.AllMuted
+    case _            => MuteSet.AllMuted
   }.onUi(m => convController.setMuted(convId, muted = m))
 }
 
-object Everything   extends BaseMenuItem(R.string.conversation__action__notifications_everything, None)
-object OnlyMentions extends BaseMenuItem(R.string.conversation__action__notifications_mentions_only, None)
-object Nothing      extends BaseMenuItem(R.string.conversation__action__notifications_nothing, None)
+object NotificationsOptionsMenuController {
+  object Everything   extends BaseMenuItem(R.string.conversation__action__notifications_everything, None)
+  object OnlyMentions extends BaseMenuItem(R.string.conversation__action__notifications_mentions_and_replies, None)
+  object Nothing      extends BaseMenuItem(R.string.conversation__action__notifications_nothing, None)
 
+  def menuItem(muteSet: MuteSet): BaseMenuItem = muteSet match {
+    case MuteSet.AllMuted            => Nothing
+    case MuteSet.OnlyMentionsAllowed => OnlyMentions
+    case _                           => Everything
+  }
+}