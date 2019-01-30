Porringion/wire-android

feat: Add Glide with stub data fetcher and basic transformations

app/src/main/java/com/waz/zclient/WireGlideModule.java
@@ -1,0 +1,38 @@
+/**
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.waz.zclient;
+
+import java.io.InputStream;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.waz.model.AssetId;
+import com.waz.zclient.glide.AssetModelLoaderFactory;
+
+@GlideModule
+public class WireGlideModule extends AppGlideModule {
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        registry.prepend(AssetId.class, InputStream.class, new AssetModelLoaderFactory(context));
+    }
+}

wireapp/wire-android_legacy

app/src/main/java/com/waz/zclient/WireGlideModule.java
@@ -1,0 +1,38 @@
+/**
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.waz.zclient;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+import com.waz.zclient.glide.AssetRequest;
+import com.waz.zclient.glide.loaders.AssetRequestModelLoaderFactory;
+
+import java.io.InputStream;
+
+@GlideModule
+public class WireGlideModule extends AppGlideModule {
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        registry.prepend(AssetRequest.class, InputStream.class, new AssetRequestModelLoaderFactory(context));
+    }
+}