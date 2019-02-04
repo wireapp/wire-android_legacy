5a296411e20486bb68818fef163e551f427bc5c8
JPonte <joao.ponte@wire.com>
2019-02-04 15:51:36 +0100

    Fix set profile picture

--- app/src/main/scala/com/waz/zclient/glide/BackgroundRequest.scala
+++ app/src/main/scala/com/waz/zclient/glide/BackgroundRequest.scala
@@ -23,28 +23,20 @@ import com.bumptech.glide.RequestBuilder
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.RequestOptions
-import com.waz.model.{AssetId, PublicAssetId}
+import com.waz.model.AssetIdGeneral
 import com.waz.zclient.glide.transformations.{BlurTransformation, DarkenTransformation, ScaleTransformation}
 
 object BackgroundRequest {
 
   val ScaleValue = 1.4f
 
-  def apply(assetId: PublicAssetId)(implicit context: Context): RequestBuilder[Drawable] =
-    backgroundRequest(GlideBuilder.apply(assetId))
-
-  def apply(assetId: AssetId)(implicit context: Context): RequestBuilder[Drawable] =
-    backgroundRequest(GlideBuilder.apply(assetId))
-
-  private def backgroundRequest(request: RequestBuilder[Drawable])(implicit context: Context) = {
+  def apply(assetId: AssetIdGeneral)(implicit context: Context): RequestBuilder[Drawable] = {
     val opt = new RequestOptions()
     opt.transforms(new CenterCrop(),
       new ScaleTransformation(ScaleValue),
       new BlurTransformation(),
       new DarkenTransformation(148, 2f))
 
-    request.apply(opt).transition(DrawableTransitionOptions.withCrossFade())
+    GlideBuilder.apply(assetId).apply(opt).transition(DrawableTransitionOptions.withCrossFade())
   }
-
-
 }
