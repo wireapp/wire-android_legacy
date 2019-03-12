fc074ec439732089eba5475e6b224852ae141a8b
John Nguyen <johnxuannguyen@gmail.com>
2019-03-12 10:33:52 +0100

    Update ReadReceiptsPartView

--- app/src/main/scala/com/waz/zclient/messages/parts/ReadReceiptsPartView.scala
+++ app/src/main/scala/com/waz/zclient/messages/parts/ReadReceiptsPartView.scala
@@ -20,19 +20,23 @@ package com.waz.zclient.messages.parts
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
-import com.waz.ZLog.ImplicitTag.implicitLogTag
 import com.waz.api.Message.Type._
-import com.waz.log.ZLog2._
+import com.waz.log.BasicLogging.LogTag.DerivedLogTag
 import com.waz.utils.events.Signal
 import com.waz.utils.returning
+import com.waz.zclient.log.LogUI._
 import com.waz.zclient.messages.UsersController.DisplayName.{Me, Other}
 import com.waz.zclient.messages.{MessageViewPart, MsgPart, SystemMessageView, UsersController}
 import com.waz.zclient.paintcode.ViewWithColor
-import com.waz.zclient.utils.ContextUtils.{getString, getColor}
-import com.waz.zclient.log.LogShowInstancesUI._
+import com.waz.zclient.utils.ContextUtils.{getColor, getString}
 import com.waz.zclient.{R, ViewHelper}
 
-class ReadReceiptsPartView(context: Context, attrs: AttributeSet, style: Int) extends LinearLayout(context, attrs, style) with MessageViewPart with ViewHelper {
+class ReadReceiptsPartView(context: Context, attrs: AttributeSet, style: Int)
+  extends LinearLayout(context, attrs, style)
+    with MessageViewPart
+    with ViewHelper
+    with DerivedLogTag {
+  
   def this(context: Context, attrs: AttributeSet) = this(context, attrs, 0)
   def this(context: Context) = this(context, null, 0)
 
