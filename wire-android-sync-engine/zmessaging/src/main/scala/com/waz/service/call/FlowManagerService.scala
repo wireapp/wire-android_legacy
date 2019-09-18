0d1dd13efe36d87b92a027ec8e560c2fd7d54094
Marco Conti <marcoconti83@gmail.com>
2019-09-18 08:53:35 +0200

    More robust emulation detection

--- wire-android-sync-engine/zmessaging/src/main/scala/com/waz/service/call/FlowManagerService.scala
+++ wire-android-sync-engine/zmessaging/src/main/scala/com/waz/service/call/FlowManagerService.scala
@@ -63,8 +63,8 @@ class DefaultFlowManagerService(context:      Context,
   }
 
   lazy val flowManager: Option[FlowManager] = Try {
-    Build.PRODUCT match {
-      case "sdk_gphone_x86" =>
+    FlowManagerService.isEmulator match {
+      case true =>
         warn(l"Emulator detected, skipping FlowManager initialization")
         null
       case _ =>
@@ -144,4 +144,14 @@ class DefaultFlowManagerService(context:      Context,
 
 object FlowManagerService {
   case class VideoCaptureDevice(id: String, name: String)
+
+  def isEmulator =
+    Build.FINGERPRINT.startsWith("generic") ||
+      Build.FINGERPRINT.startsWith("unknown") ||
+      Build.MODEL.contains("google_sdk") ||
+      Build.MODEL.contains("Emulator") ||
+      Build.MODEL.contains ("Android SDK built for x86") ||
+      Build.MANUFACTURER.contains ("Genymotion") ||
+      (Build.BRAND.startsWith ("generic") && Build.DEVICE.startsWith ("generic") ) ||
+      "google_sdk" == Build.PRODUCT
 }
