7f24c7edba48413972bd5f6017ee049e40733edb
Gizem Burhanoglu <gizembrh@gmail.com>
2019-10-25 11:03:40 +0200

    Add tests for TeamsService

--- wire-android-sync-engine/zmessaging/src/test/scala/com/waz/service/teams/TeamsServiceSpec.scala
+++ wire-android-sync-engine/zmessaging/src/test/scala/com/waz/service/teams/TeamsServiceSpec.scala
@@ -17,18 +17,20 @@
  */
 package com.waz.service.teams
 
+import com.waz.api.ErrorType
+import com.waz.api.impl.ErrorResponse
+import com.waz.content.UserPreferences.{CopyPermissions, SelfPermissions}
 import com.waz.content._
 import com.waz.log.BasicLogging.LogTag.DerivedLogTag
 import com.waz.model._
-import com.waz.service.{ErrorsService, SearchKey}
 import com.waz.service.conversation.{ConversationsContentUpdater, ConversationsService}
+import com.waz.service.{ErrorsService, SearchKey}
 import com.waz.specs.AndroidFreeSpec
+import com.waz.sync.client.TeamsClient
+import com.waz.sync.client.TeamsClient.TeamMember
 import com.waz.sync.{SyncRequestService, SyncServiceHandle}
 import com.waz.testutils.TestUserPreferences
 import com.waz.utils.events.EventStream
-import com.waz.content.UserPreferences.{CopyPermissions, SelfPermissions}
-import com.waz.sync.client.TeamsClient
-import com.waz.sync.client.TeamsClient.TeamMember
 
 import scala.collection.breakOut
 import scala.concurrent.Future
@@ -233,6 +235,54 @@ class TeamsServiceSpec extends AndroidFreeSpec with DerivedLogTag {
     // THEN
     result(userPrefs(SelfPermissions).apply()) shouldEqual 0
     result(userPrefs(CopyPermissions).apply()) shouldEqual 0
+  }
+
+  feature("Delete group conversation") {
+    scenario("Delete group conversation should delete conversation from storage") {
+      //GIVEN
+      val teamId = TeamId()
+      val rConvId = RConvId()
+      val service = createService
+
+      (sync.deleteGroupConversation _).expects(teamId, rConvId).anyNumberOfTimes().onCall { (_, rId) =>
+        service.onGroupConversationDeleted(rId)
+        Future.successful(SyncId())
+      }
+
+      //EXPECT
+      (convsService.deleteConversation _).expects(rConvId).once()
+
+      //WHEN
+      result(service.deleteGroupConversation(teamId, rConvId))
+    }
+
+    //FIXME: gets success individually but failing on full test suite
+    ignore("When delete group conversation request returns error, post error to ui") {
+      //GIVEN
+      val teamId = TeamId()
+      val convId = ConvId()
+      val rConvId = RConvId()
+      val conversationData = ConversationData(convId, rConvId)
+      val service = createService
+
+      (convsContent.convByRemoteId _).expects(rConvId).anyNumberOfTimes()
+        .returning(Future.successful(Some(conversationData)))
+
+      val errorResponse = ErrorResponse(404, message = "not found", label = "")
+
+      (sync.deleteGroupConversation _).expects(teamId, rConvId).anyNumberOfTimes().onCall { (_, rId) =>
+        service.onGroupConversationDeleteError(errorResponse, rId)
+        Future.successful(SyncId())
+      }
+
+      //EXPECT
+      (errorsService.addErrorWhenActive _).expects(where { errorData: ErrorData =>
+        errorData.errType == ErrorType.CANNOT_DELETE_GROUP_CONVERSATION && errorData.convId.contains(convId)
+      }).once()
+
+      //WHEN
+      result(service.deleteGroupConversation(teamId, rConvId))
+    }
   }
 
   def createService = {
