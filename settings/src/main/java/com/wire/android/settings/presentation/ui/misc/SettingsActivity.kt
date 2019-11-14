d92769a7ba2df2095f7fca35a71805f9c087385a
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2019-11-14 18:06:38 +0100

    feat: setup settings module

---
+++ settings/src/main/java/com/wire/android/settings/presentation/ui/misc/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.wire.android.settings.presentation.ui.misc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
