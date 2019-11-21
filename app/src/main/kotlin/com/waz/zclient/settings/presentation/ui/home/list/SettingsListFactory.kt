9d4ef46ae9a583e6c8dec78e227bc1571afa0763
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2019-11-21 17:03:04 +0100

    configure developer settings

--- app/src/main/kotlin/com/waz/zclient/settings/presentation/ui/home/list/SettingsListFactory.kt
+++ app/src/main/kotlin/com/waz/zclient/settings/presentation/ui/home/list/SettingsListFactory.kt
@@ -2,17 +2,29 @@ package com.waz.zclient.settings.presentation.ui.home.list
 
 import com.waz.zclient.R
 import com.waz.zclient.settings.presentation.model.SettingsItem
+import com.waz.zclient.utilities.config.ConfigHelper
 import com.waz.zclient.utilities.resources.ResourceManager
 
 class SettingsListFactory {
 
     companion object {
+
+        private val configHelper: ConfigHelper = ConfigHelper()
+
         fun generateList(resourceManager: ResourceManager): List<SettingsItem> {
             val titles = resourceManager.getStringArray(R.array.settings_titles)
             val icons = resourceManager.getStringArray(R.array.settings_icons)
+            val developerTitles = resourceManager.getStringArray(R.array.settings_developer_titles)
+            val developerIcons = resourceManager.getStringArray(R.array.settings_developer_icons)
+
             val settingItems = ArrayList<SettingsItem>()
             for (i in titles.indices) {
                 settingItems.add(SettingsItem(titles[i], icons[i]))
+            }
+            if (configHelper.isDeveloperSettingsEnabled()) {
+                for (i in developerTitles.indices) {
+                    settingItems.add(SettingsItem(developerTitles[i], developerIcons[i]))
+                }
             }
             return settingItems
         }
--- app/src/main/kotlin/com/waz/zclient/settings/presentation/ui/home/list/SettingsViewHolder.kt
+++ app/src/main/kotlin/com/waz/zclient/settings/presentation/ui/home/list/SettingsViewHolder.kt
@@ -9,15 +9,8 @@ import com.waz.zclient.utilities.config.ConfigHelper
 import kotlinx.android.synthetic.main.item_settings.view.*
 
 class SettingsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val configHelper: ConfigHelper = ConfigHelper()
-
     fun bind(settingsItem: SettingsItem) {
         itemView.settings_item_title.text = settingsItem.title
         itemView.settings_item_icon.text = settingsItem.icon
-
-        when (configHelper.isDeveloperSettingsEnabled()) {
-            true -> itemView.visibility = VISIBLE
-            false -> itemView.visibility = GONE
-        }
     }
 }
