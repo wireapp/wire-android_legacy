5a319c6fd936f6a211471ae0e55463847cc53e13
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2019-11-28 18:09:50 +0100

    fix wrong imports for kotlin ui tests

--- app/src/androidTest/kotlin/com/waz/zclient/markdown/StyleSheetTest.kt
+++ app/src/androidTest/kotlin/com/waz/zclient/markdown/StyleSheetTest.kt
@@ -17,7 +17,7 @@
  */
 package com.waz.zclient.markdown
 
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.waz.zclient.markdown.spans.commonmark.*
 import org.commonmark.node.*
 import org.junit.Assert.*
