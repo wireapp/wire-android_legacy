c2d964c17a418932ff85bc6c29676aa383f5fe0a
BradleyWire <bradley.wilson@wire.com>
2019-11-29 11:51:20 +0100

    fixed getSelfClientUseCase

--- app/src/main/kotlin/com/waz/zclient/devices/domain/GetSelfClientIdUseCase.kt
+++ app/src/main/kotlin/com/waz/zclient/devices/domain/GetSelfClientIdUseCase.kt
@@ -1,12 +1,13 @@
 package com.waz.zclient.devices.domain
 
-import com.waz.zclient.core.usecase.coroutines.Either
+import com.waz.zclient.core.data.source.remote.RequestResult
 import com.waz.zclient.core.usecase.coroutines.UseCase
 
 class GetSelfClientIdUseCase() : UseCase<String, Unit>() {
 
-    override suspend fun run(params: Unit): Either<Failure, String> {
+    override suspend fun run(params: Unit): RequestResult<String> {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
 
 }
