dca024d3c14ef2fa9d82394954130d0e2e9ee956
Gizem Burhanoglu <gizembrh@gmail.com>
2019-12-02 17:58:45 +0100

    Change KeyValue naming to UserPreferences and bind dao to db

--- roomdb/src/main/kotlin/com.wire.roomdb/UserDatabase.kt
+++ roomdb/src/main/kotlin/com.wire.roomdb/UserDatabase.kt
@@ -1,7 +1,30 @@
 package com.wire.roomdb
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = arrayOf(KeyValue::class), version = 1)
-abstract class UserDatabase : RoomDatabase()
+@Database(entities = [UserPreference::class], version = 1)
+abstract class UserDatabase : RoomDatabase() {
+
+    abstract fun userPreferencesDao(): UserPreferencesDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE_MAP : MutableMap<String, UserDatabase> = mutableMapOf()
+
+        //TODO: better name for db
+        @JvmStatic
+        fun getInstance(context: Context, userId: String): UserDatabase =
+            INSTANCE_MAP[userId] ?: synchronized(this) {
+                INSTANCE_MAP[userId] ?: buildDatabase(context, "NEW_DB_$userId").also {
+                    INSTANCE_MAP[userId] = it
+                }
+            }
+
+        private fun buildDatabase(context: Context, dbName: String): UserDatabase =
+            Room.databaseBuilder(context.applicationContext, UserDatabase::class.java, dbName).build()
+    }
+
+}
--- roomdb/src/main/kotlin/com.wire.roomdb/UserPreference.kt
+++ roomdb/src/main/kotlin/com.wire.roomdb/UserPreference.kt
@@ -4,8 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "KeyValues")
-data class KeyValue(
+@Entity(tableName = "UserPreferences")
+data class UserPreference(
     @PrimaryKey @ColumnInfo(name = "key") val key: String,
     @ColumnInfo(name = "value") val value: String
 )
