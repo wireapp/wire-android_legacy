82c89706fbc3856021f355e9d6bbbf9264bd8b07
Fernando Cejas <fcejas@gmail.com>
2019-12-09 18:25:31 +0100

    Add network class with an api request method.

---
+++ app/src/main/kotlin/com/waz/zclient/core/network/Network.kt
@@ -0,0 +1,23 @@
+package com.waz.zclient.core.network
+
+import com.waz.zclient.core.exception.Failure
+import com.waz.zclient.core.exception.Failure.ServerError
+import com.waz.zclient.core.functional.Either
+import com.waz.zclient.core.functional.Either.Left
+import com.waz.zclient.core.functional.Either.Right
+import retrofit2.Call
+
+class Network {
+
+    fun <T, R> request(call: Call<T>, transform: (T) -> R, default: T): Either<Failure, R> {
+        return try {
+            val response = call.execute()
+            when (response.isSuccessful) {
+                true -> Right(transform((response.body() ?: default)))
+                false -> Left(ServerError)
+            }
+        } catch (exception: Throwable) {
+            Left(ServerError)
+        }
+    }
+}
---
+++ app/src/test/kotlin/com/waz/zclient/core/network/NetworkTest.kt
@@ -0,0 +1,13 @@
+package com.waz.zclient.core.network
+
+import org.junit.Before
+
+class NetworkTest {
+
+    private lateinit var network: Network
+
+    @Before
+    fun setUp() {
+        network = Network()
+    }
+}
