44be49bdec32eff71bdc82bb5d4f91c5ea936848
Fernando Cejas <fcejas@gmail.com>
2019-12-09 18:32:35 +0100

    Add UnitTest inheritance to Network class.

--- app/src/test/kotlin/com/waz/zclient/core/network/NetworkTest.kt
+++ app/src/test/kotlin/com/waz/zclient/core/network/NetworkTest.kt
@@ -1,8 +1,9 @@
 package com.waz.zclient.core.network
 
+import com.waz.zclient.UnitTest
 import org.junit.Before
 
-class NetworkTest {
+class NetworkTest : UnitTest() {
 
     private lateinit var network: Network
 
