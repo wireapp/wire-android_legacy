fac6b171f4ee56ad4aebcdb750d5a48dcfbf78da
Fernando Cejas <fcejas@gmail.com>
2019-12-10 17:52:32 +0100

    Add Network classes and Repository Sample.

--- app/src/main/kotlin/com/waz/zclient/core/network/Service.kt
+++
@@ -1,4 +0,0 @@
-package com.waz.zclient.core.network
-
-interface Service {
-}
--- app/src/main/kotlin/com/waz/zclient/core/network/api/client/ClientsService.kt
+++ app/src/main/kotlin/com/waz/zclient/core/network/api/client/ClientsService.kt
@@ -1,13 +1,20 @@
 package com.waz.zclient.core.network.api.client
 
+import com.waz.zclient.core.exception.Failure
+import com.waz.zclient.core.functional.Either
+import com.waz.zclient.core.network.ApiService
+import com.waz.zclient.di.Injector
 import com.waz.zclient.features.clients.ClientEntity
-import retrofit2.Call
 import retrofit2.Retrofit
 
-class ClientsService(private val retrofit: Retrofit): ClientsApi {
+class ClientsService(private val retrofit: Retrofit) : ApiService(Injector.networkHandler()) {
 
     private val clientsApi by lazy { retrofit.create(ClientsApi::class.java) }
 
-    override fun allClients(): Call<List<ClientEntity>> = clientsApi.allClients()
-    override fun clientById(clientId: String?): Call<ClientEntity> = clientsApi.clientById(clientId)
+    fun allClients(): Either<Failure, List<ClientEntity>> =
+        request(clientsApi.allClients(), emptyList())
+
+    fun clientById(clientId: String?): Either<Failure, ClientEntity> =
+        request(clientsApi.clientById(clientId), ClientEntity.empty())
 }
+
