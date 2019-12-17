1c86136879b6c6b7146cea363a67555a81d17929
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2019-12-17 13:09:45 +0100

    revert database version fix migration issue

--- storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/UserDatabaseMigration.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/UserDatabaseMigration.kt
@@ -3,13 +3,13 @@ package com.waz.zclient.storage.db.users.migration
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-class UserDatabaseMigration : Migration(125, 126) {
+class UserDatabaseMigration : Migration(124, 125) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("CREATE TABLE '$NEW_CLIENT_TABLE_NAME' ('$CLIENT_ID_KEY' TEXT NOT NULL, '$NEW_CLIENT_TIME_KEY' TEXT NOT NULL,  '$CLIENT_LABEL_KEY' TEXT NOT NULL, '$NEW_CLIENT_TYPE_KEY' TEXT NOT NULL, '$CLIENT_CLASS_KEY' TEXT NOT NULL ,'$CLIENT_MODEL_KEY' TEXT NOT NULL,  '$CLIENT_LOCATION_LAT_KEY' REAL NOT NULL,  '$CLIENT_LOCATION_LONG_KEY' REAL NOT NULL, '$NEW_CLIENT_LOCATION_NAME_KEY' TEXT,  '$CLIENT_VERIFICATION_KEY' TEXT NOT NULL,  '$CLIENT_ENC_KEY' TEXT NOT NULL,  '$CLIENT_MAC_KEY' TEXT NOT NULL,  PRIMARY KEY('$CLIENT_ID_KEY'))")
 
         // "KeyValues" to "user_preference" Migration
         database.execSQL("CREATE TABLE IF NOT EXISTS `$USER_PREFERENCE_TABLE_NAME` (`key` TEXT PRIMARY KEY NOT NULL ,`value` TEXT)")
-//        database.execSQL("INSERT INTO $USER_PREFERENCE_TABLE_NAME SELECT * FROM $KEY_VALUES_TABLE_NAME")
+        database.execSQL("INSERT INTO $USER_PREFERENCE_TABLE_NAME SELECT * FROM $KEY_VALUES_TABLE_NAME")
 //        database.execSQL("DROP TABLE $KEY_VALUES_TABLE_NAME")
 
         // "Users" to "user" Migration
@@ -18,7 +18,7 @@ class UserDatabaseMigration : Migration(125, 126) {
             "`conn_timestamp` INTEGER , `conn_msg` TEXT , `conversation` TEXT , `relation` TEXT , `timestamp` INTEGER , `display_name` TEXT ," +
             " `verified` TEXT , `deleted` INTEGER NOT NULL , `availability` INTEGER , `handle` TEXT , `provider_id` TEXT , `integration_id` TEXT ," +
             " `expires_at` INTEGER , `managed_by` TEXT , `self_permissions` INTEGER , `copy_permissions` INTEGER , `created_by` TEXT )")
-//        database.execSQL("INSERT INTO $USER_TABLE_NAME SELECT * FROM $USERS_TABLE_NAME")
+        database.execSQL("INSERT INTO $USER_TABLE_NAME SELECT * FROM $USERS_TABLE_NAME")
 //        database.execSQL("DROP TABLE $USERS_TABLE_NAME")
     }
 
