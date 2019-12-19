85e940c071d3c4afeb9944e6299e43fa658edc3f
Fernando Cejas <fcejas@gmail.com>
2019-12-19 18:07:01 +0100

    Kotlin core network rewrite part 2 (#2468)
    * Prepare unit testing environment and add Either class.
    
    * Add useful extension functions and a NetworkHandler for checking device active connection.
    
    * Add useful extension functions and a NetworkHandler for checking device active connection.
    
    * Add network class with an api request method.
    
    * Add UnitTest inheritance to Network class.
    
    * Add Api, Network and Service classes. Plus Temporary Injector.
    
    * Add Network classes and Repository Sample.
    
    * Add error handling.
    
    * Add Logic for toke refresh. Still a lot TODO.
    
    * First attempt to test netwroking. Tiny refactor.
    
    * Fix unit test compile errors
    
    * Ignore ClientsServiceTest for now since it depends on Android frameworks

--- app/src/main/kotlin/com/waz/zclient/core/exception/Failure.kt
+++ app/src/main/kotlin/com/waz/zclient/core/exception/Failure.kt
@@ -4,16 +4,23 @@ package com.waz.zclient.core.exception
  * Base Class for handling errors/failures/exceptions.
  * Every feature specific failure should extend [FeatureFailure] class.
  */
-sealed class Failure {
-    // Server Connection Failures
-    object NoNetworkConnection : Failure()
-    object ServerError : Failure()
-    object BadRequest : Failure()
-    object Unauthorized : Failure()
-    object Forbidden : Failure()
-    object NotFound : Failure()
-    object InternalServerError : Failure()
 
-    /** * Extend this class for feature specific failures.*/
-    abstract class FeatureFailure: Failure()
-}
+sealed class Failure
+
+sealed class NetworkFailure : Failure()
+sealed class DatabaseFailure : Failure()
+
+object NetworkConnection : NetworkFailure()
+object NetworkServiceError : NetworkFailure()
+object ServerError : NetworkFailure()
+object BadRequest : NetworkFailure()
+object Unauthorized : NetworkFailure()
+object Forbidden : NetworkFailure()
+object NotFound : NetworkFailure()
+object InternalServerError : NetworkFailure()
+data class HttpError(val errorCode: Int, val errorMessage: String) : NetworkFailure()
+
+object DatabaseError : DatabaseFailure()
+
+/** * Extend this class for feature specific failures.*/
+abstract class FeatureFailure : Failure()
--- app/src/test/kotlin/com/waz/zclient/core/network/ApiServiceTest.kt
+++ app/src/test/kotlin/com/waz/zclient/core/network/ApiServiceTest.kt
@@ -2,16 +2,23 @@ package com.waz.zclient.core.network
 
 import com.waz.zclient.UnitTest
 import org.junit.Before
+import org.junit.Test
 import org.mockito.Mock
 
 class ApiServiceTest : UnitTest() {
 
     private lateinit var apiService: ApiService
 
-    @Mock private lateinit var networkHandler: NetworkHandler
+    @Mock
+    private lateinit var networkHandler: NetworkHandler
 
     @Before
     fun setUp() {
 //        apiService = ApiService(networkHandler)
+    }
+
+    @Test
+    fun test() {
+
     }
 }
