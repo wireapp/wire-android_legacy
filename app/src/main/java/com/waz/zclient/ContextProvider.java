2438327f1bf9e45888f174bc87a7a596d4a9cf65
Gizem Burhanoglu <gizembrh@gmail.com>
2019-12-20 09:50:47 +0100

    Add ContextProvider class which was mistakenly deleted during auto-merge

---
+++ app/src/main/java/com/waz/zclient/ContextProvider.java
@@ -0,0 +1,28 @@
+/**
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.waz.zclient;
+
+import android.content.Context;
+
+//TODO: delete this when deleting com.waz.zclient.core.di.Injector.kt
+public class ContextProvider {
+
+    public static Context getApplicationContext() {
+        return WireApplication.APP_INSTANCE().getApplicationContext();
+    }
+}
