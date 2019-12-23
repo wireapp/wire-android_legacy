2a8bd2dbe0bec5646803f875c76c430a59dd12fa
Gizem Burhanoglu <gizembrh@gmail.com>
2019-12-23 11:33:02 +0100

    Add http error code anf failure matching unit tests for ApiService

--- app/src/test/kotlin/com/waz/zclient/core/network/ApiServiceTest.kt
+++ app/src/test/kotlin/com/waz/zclient/core/network/ApiServiceTest.kt
@@ -2,6 +2,11 @@ package com.waz.zclient.core.network
 
 import com.waz.zclient.UnitTest
 import com.waz.zclient.core.exception.BadRequest
+import com.waz.zclient.core.exception.Failure
+import com.waz.zclient.core.exception.Forbidden
+import com.waz.zclient.core.exception.InternalServerError
+import com.waz.zclient.core.exception.NotFound
+import com.waz.zclient.core.exception.ServerError
 import com.waz.zclient.core.exception.Unauthorized
 import com.waz.zclient.core.threading.ThreadHandler
 import com.waz.zclient.testutils.verifyLeft
@@ -54,28 +59,46 @@ class ApiServiceTest : UnitTest() {
 
     @Test
     fun `given call fails with http 400 error, then returns BadRequest failure`() {
-        `when`(networkHandler.isConnected).thenReturn(true)
-        doNothing().`when`(threadHandler).failFastIfUIThread()
-
-        val (call, _) = prepareResponseError<String>(400)
-
-        val result = apiService.request(call, "")
-
-        result.isLeft shouldBe true
-        result verifyLeft BadRequest
+        testHttpFailureMapping(400, BadRequest)
     }
 
     @Test
     fun `given call fails with http 401 error, then returns Unauthorized failure`() {
+        testHttpFailureMapping(401, Unauthorized)
+    }
+
+    @Test
+    fun `given call fails with http 403 error, then returns Forbidden failure`() {
+        testHttpFailureMapping(403, Forbidden)
+    }
+
+    @Test
+    fun `given call fails with http 404 error, then returns NotFound failure`() {
+        testHttpFailureMapping(404, NotFound)
+    }
+
+    @Test
+    fun `given call fails with http 500 error, then returns InternalServerError failure`() {
+        testHttpFailureMapping(500, InternalServerError)
+    }
+
+    @Test
+    fun `given call fails with any other error, then returns ServerError failure`() {
+        testHttpFailureMapping(-1, ServerError)
+    }
+
+    private fun testHttpFailureMapping(httpErrorCode: Int, failure: Failure) {
         `when`(networkHandler.isConnected).thenReturn(true)
         doNothing().`when`(threadHandler).failFastIfUIThread()
 
-        val (call, _) = prepareResponseError<String>(401)
+        val (call, response) = prepareResponse<String>()
+        `when`(response.isSuccessful).thenReturn(false)
+        `when`(response.code()).thenReturn(httpErrorCode)
 
         val result = apiService.request(call, "")
 
         result.isLeft shouldBe true
-        result verifyLeft Unauthorized
+        result verifyLeft failure
     }
 
     //endregion
@@ -85,13 +108,6 @@ class ApiServiceTest : UnitTest() {
         val call: Call<T> = mock(Call::class.java) as Call<T>
         val response: Response<T> = mock(Response::class.java) as Response<T>
         `when`(call.execute()).thenReturn(response)
-        return call to response
-    }
-
-    private fun <T> prepareResponseError(httpErrorCode: Int): Pair<Call<T>, Response<T>> {
-        val (call, response) = prepareResponse<T>()
-        `when`(response.isSuccessful).thenReturn(false)
-        `when`(response.code()).thenReturn(httpErrorCode)
         return call to response
     }
 
