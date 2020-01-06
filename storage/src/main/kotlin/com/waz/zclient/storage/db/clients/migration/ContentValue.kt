cd52b9744f5fceed568ab2679e9a1165a6423395
Maciej Gorywoda <makingthematrix@gmail.com>
2020-01-06 15:38:04 +0100

    remove unused code

--- storage/src/main/kotlin/com/waz/zclient/storage/db/clients/migration/ContentValue.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/clients/migration/ContentValue.kt
@@ -4,10 +4,4 @@ sealed class ContentValue {
     data class StringValue(val value: String): ContentValue()
     data class LongValue(val value: Long): ContentValue()
     data class DoubleValue(val value: Double): ContentValue()
-
-    companion object {
-        fun apply(value: String): StringValue = StringValue(value)
-        fun apply(value: Long):   LongValue   = LongValue(value)
-        fun apply(value: Double): DoubleValue = DoubleValue(value)
-    }
 }
