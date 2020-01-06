dfc70683e72d547e19597ed9493b583fb23073fa
Fernando Cejas <fcejas@gmail.com>
2020-01-06 14:15:20 +0100

    Bump android client version.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     //wire android client
-    const val ANDROID_CLIENT_MAJOR_VERSION = "3.43."
+    const val ANDROID_CLIENT_MAJOR_VERSION = "3.44."
 
     //core
     const val KOTLIN = "1.3.60"
