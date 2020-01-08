094f45b61a17b358d6fd1f35180d21d83b26d251
Gizem Burhanoglu <gizembrh@gmail.com>
2020-01-08 19:22:26 +0100

    Simplify logic for updating refresh token with response header

--- app/src/main/kotlin/com/waz/zclient/core/network/AccessTokenInterceptor.kt
+++ app/src/main/kotlin/com/waz/zclient/core/network/AccessTokenInterceptor.kt
@@ -6,12 +6,18 @@ import okhttp3.Response
 
 class AccessTokenInterceptor(private val authTokenHandler: AuthTokenHandler) : Interceptor {
 
+    companion object {
+        private const val RESPONSE_HEADER_REFRESH_TOKEN_KEY = "Cookie"
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val token = authTokenHandler.accessToken()
 
         return when (token != String.empty()) {
             true -> addAuthHeader(chain, token)
             false -> chain.proceed(chain.request())
+        }.also {
+            updateRefreshTokenFromResponse(it)
         }
     }
 
@@ -23,4 +29,11 @@ class AccessTokenInterceptor(private val authTokenHandler: AuthTokenHandler) : I
             .build()
         return chain.proceed(authenticatedRequest)
     }
+
+    private fun updateRefreshTokenFromResponse(response: Response) =
+        response.headers()[RESPONSE_HEADER_REFRESH_TOKEN_KEY]?.let {
+            if (authTokenHandler.refreshToken() != it) {
+                authTokenHandler.updateRefreshToken(it)
+            }
+        }
 }
--- app/src/test/kotlin/com/waz/zclient/core/network/AccessTokenInterceptorTest.kt
+++ app/src/test/kotlin/com/waz/zclient/core/network/AccessTokenInterceptorTest.kt
@@ -1,13 +1,14 @@
 package com.waz.zclient.core.network
 
 import com.waz.zclient.UnitTest
+import com.waz.zclient.any
 import com.waz.zclient.core.extension.empty
+import okhttp3.Headers
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
 import org.junit.Before
 import org.junit.Test
-import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.Mockito.mock
@@ -22,43 +23,40 @@ class AccessTokenInterceptorTest : UnitTest() {
 
     private lateinit var accessTokenInterceptor: AccessTokenInterceptor
 
+    @Mock
+    private lateinit var chain: Interceptor.Chain
+
+    @Mock
+    private lateinit var request: Request
+
     @Before
     fun setUp() {
+        `when`(chain.request()).thenReturn(request)
         accessTokenInterceptor = AccessTokenInterceptor(authTokenHandler)
     }
 
     @Test
     fun `if there's an access token, adds it to request's header`() {
-        `when`(authTokenHandler.accessToken()).thenReturn("testToken")
-
-        val chain = mock(Interceptor.Chain::class.java)
-        val initialRequest = mock(Request::class.java)
-        `when`(chain.request()).thenReturn(initialRequest)
+        `when`(authTokenHandler.accessToken()).thenReturn(ACCESS_TOKEN)
 
         val requestBuilder = mock(Request.Builder::class.java)
-        `when`(initialRequest.newBuilder()).thenReturn(requestBuilder)
-        `when`(requestBuilder.addHeader(any(), any())).thenReturn(requestBuilder)
-
-        val requestWithHeader = mock(Request::class.java)
-        `when`(requestBuilder.build()).thenReturn(requestWithHeader)
-
-        `when`(chain.proceed(requestWithHeader)).thenReturn(mock(Response::class.java))
+        val requestWithHeader = mockRequestWithAuthHeader(requestBuilder)
+        val response = mockResponse()
+        `when`(chain.proceed(requestWithHeader)).thenReturn(response)
 
         accessTokenInterceptor.intercept(chain)
 
         verify(authTokenHandler).accessToken()
-        verify(requestBuilder).addHeader("Authorization", "Bearer testToken")
+        verify(requestBuilder).addHeader("Authorization", "Bearer $ACCESS_TOKEN")
         verify(chain).proceed(requestWithHeader)
         verify(requestBuilder, never()).removeHeader("Authorization")
     }
 
     @Test
     fun `if access token is empty, doesn't add authorization header`() {
-        val chain = mock(Interceptor.Chain::class.java)
-        val request = mock(Request::class.java)
         `when`(authTokenHandler.accessToken()).thenReturn(String.empty())
-        `when`(chain.request()).thenReturn(request)
-        `when`(chain.proceed(request)).thenReturn(mock(Response::class.java))
+        val response = mockResponse()
+        `when`(chain.proceed(request)).thenReturn(response)
 
         accessTokenInterceptor.intercept(chain)
 
@@ -67,4 +65,80 @@ class AccessTokenInterceptorTest : UnitTest() {
         verifyNoMoreInteractions(request)
     }
 
+    @Test
+    fun `when response returns a "Cookie" header different than current refresh token, updates refresh token`() {
+        `when`(authTokenHandler.accessToken()).thenReturn(ACCESS_TOKEN)
+        `when`(authTokenHandler.refreshToken()).thenReturn(REFRESH_TOKEN)
+        val requestWithHeader = mockRequestWithAuthHeader()
+        val response = mockResponse(cookie = NEW_REFRESH_TOKEN)
+        `when`(chain.proceed(requestWithHeader)).thenReturn(response)
+
+        accessTokenInterceptor.intercept(chain)
+
+        verify(authTokenHandler).accessToken()
+        verify(response).headers()
+        verify(authTokenHandler).refreshToken()
+        verify(authTokenHandler).updateRefreshToken(NEW_REFRESH_TOKEN)
+        verifyNoMoreInteractions(authTokenHandler)
+    }
+
+    @Test
+    fun `when response returns a "Cookie" header same as current refresh token, does not update refresh token`() {
+        `when`(authTokenHandler.accessToken()).thenReturn(ACCESS_TOKEN)
+        `when`(authTokenHandler.refreshToken()).thenReturn(REFRESH_TOKEN)
+        val requestWithHeader = mockRequestWithAuthHeader()
+        val response = mockResponse(cookie = REFRESH_TOKEN)
+        `when`(chain.proceed(requestWithHeader)).thenReturn(response)
+
+        accessTokenInterceptor.intercept(chain)
+
+        verify(authTokenHandler).accessToken()
+        verify(response).headers()
+        verify(authTokenHandler).refreshToken()
+        verify(authTokenHandler, never()).updateRefreshToken(any())
+        verifyNoMoreInteractions(authTokenHandler)
+    }
+
+    @Test
+    fun `when response does not return a "Cookie" header, does not attempt to update refresh token`() {
+        `when`(authTokenHandler.accessToken()).thenReturn(ACCESS_TOKEN)
+        val requestWithHeader = mockRequestWithAuthHeader()
+        val response = mockResponse()
+        `when`(chain.proceed(requestWithHeader)).thenReturn(response)
+
+        accessTokenInterceptor.intercept(chain)
+
+        verify(authTokenHandler).accessToken()
+        verify(response).headers()
+        verify(authTokenHandler, never()).refreshToken()
+        verify(authTokenHandler, never()).updateRefreshToken(any())
+        verifyNoMoreInteractions(authTokenHandler)
+    }
+
+    private fun mockRequestWithAuthHeader(
+        requestBuilder: Request.Builder = mock(Request.Builder::class.java)
+    ): Request {
+        `when`(request.newBuilder()).thenReturn(requestBuilder)
+        `when`(requestBuilder.addHeader(any(), any())).thenReturn(requestBuilder)
+
+        val requestWithHeader = mock(Request::class.java)
+        `when`(requestBuilder.build()).thenReturn(requestWithHeader)
+
+        return requestWithHeader
+    }
+
+    private fun mockResponse(cookie: String? = null): Response {
+        val response = mock(Response::class.java)
+        val headers = mock(Headers::class.java)
+        `when`(response.headers()).thenReturn(headers)
+        `when`(headers[COOKIE_HEADER_NAME]).thenReturn(cookie)
+        return response
+    }
+
+    companion object {
+        private const val ACCESS_TOKEN = "accessToken"
+        private const val REFRESH_TOKEN = "refreshToken"
+        private const val NEW_REFRESH_TOKEN = "newRefreshToken"
+        private const val COOKIE_HEADER_NAME = "Cookie"
+    }
 }
