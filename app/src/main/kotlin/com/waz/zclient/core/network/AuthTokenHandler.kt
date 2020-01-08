4c74f0e350cf973285992f9edca4053a8330d3dd
Gizem Burhanoglu <gizembrh@gmail.com>
2020-01-08 17:02:17 +0100

    Revert "Add renewing refresh token from response header logic"
    This reverts commit 0bbabc5179bf3c70e80ded9e5c1a25ed0a5ce6aa.

--- app/src/main/kotlin/com/waz/zclient/core/network/AuthTokenHandler.kt
+++ app/src/main/kotlin/com/waz/zclient/core/network/AuthTokenHandler.kt
@@ -3,13 +3,10 @@ package com.waz.zclient.core.network
 import com.waz.zclient.core.exception.Failure
 import com.waz.zclient.core.functional.Either
 import com.waz.zclient.core.functional.map
-import retrofit2.Response
 
 class AuthTokenHandler(private val tokenRepository: AccessTokenRepository) {
 
     companion object {
-        private const val RESPONSE_HEADER_REFRESH_TOKEN_KEY = "Cookie"
-
         const val AUTH_HEADER = "Authorization"
         const val AUTH_HEADER_TOKEN_TYPE = "Bearer"
     }
@@ -19,13 +16,6 @@ class AuthTokenHandler(private val tokenRepository: AccessTokenRepository) {
 
     fun refreshToken() = tokenRepository.refreshToken()
     fun updateRefreshToken(newRefreshToken: String) = tokenRepository.updateRefreshToken(newRefreshToken)
-
-    fun updateRefreshTokenFromResponse(response: Response<*>) =
-        response.headers()[RESPONSE_HEADER_REFRESH_TOKEN_KEY]?.let {
-            if (refreshToken() != it) {
-                updateRefreshToken(it)
-            }
-        }
 
     fun renewAccessToken(refreshToken: String): Either<Failure, String> =
         tokenRepository.renewAccessToken(refreshToken).map { it.token }
