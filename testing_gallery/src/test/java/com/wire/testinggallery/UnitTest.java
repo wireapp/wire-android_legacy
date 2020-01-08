7434c6ce91314d403b17ca4cd523764c0b3cc420
Fernando Cejas <fcejas@gmail.com>
2020-01-08 18:50:59 +0100

    Add Test Rule to Unit Test class for junit tests.

--- testing_gallery/src/test/java/com/wire/testinggallery/UnitTest.java
+++ testing_gallery/src/test/java/com/wire/testinggallery/UnitTest.java
@@ -1,11 +1,21 @@
 package com.wire.testinggallery;
 
+import org.junit.Rule;
+import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 /**
- * Base class for Unit tests. Inherit from it to create test cases which DO NOT contain android
- * framework dependencies or components.
+ * Base class for Unit tests. Inherit from it to create test cases which DO NOT
+ * contain android framework dependencies or components. 
  */
 @RunWith(MockitoJUnitRunner.class)
-public abstract class UnitTest { }
+public abstract class UnitTest {
+
+    @Rule
+    public TestRule injectMocksRule = (base, description) -> {
+        MockitoAnnotations.initMocks(UnitTest.this);
+        return base;
+    };
+}
