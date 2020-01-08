a8825fa167ea389733fd8d6322429ddf87c97ed5
Gizem Burhanoglu <gizembrh@gmail.com>
2020-01-08 11:42:13 +0100

    Simplify RawResponseRegistry class implementation

--- app/src/main/kotlin/com/waz/zclient/core/network/RawResponseRegistry.kt
+++ app/src/main/kotlin/com/waz/zclient/core/network/RawResponseRegistry.kt
@@ -4,21 +4,13 @@ import retrofit2.Response
 
 class RawResponseRegistry {
 
-    private val listeners = mutableSetOf<RawResponseListener>()
+    private val listeners = mutableSetOf<Function1<Response<*>, Unit>>()
 
     fun addRawResponseAction(action: (Response<*>) -> Unit) {
-        listeners.add(object : RawResponseListener {
-            override fun onRawResponseReceived(response: Response<*>) {
-                action(response)
-            }
-        })
+        listeners.add(action)
     }
 
     fun notifyRawResponseReceived(response: Response<*>) {
-        listeners.forEach { it.onRawResponseReceived(response) }
-    }
-
-    private interface RawResponseListener {
-        fun onRawResponseReceived(response: Response<*>)
+        listeners.forEach { it.invoke(response) }
     }
 }
