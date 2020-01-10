188b6b034f196ae23e59322f17ae8b40cba8152f
BradleyWire <bradley.wilson@wire.com>
2020-01-10 11:02:38 +0100

    Updated missing test cases for UserRemoteDataSource

--- app/src/main/kotlin/com/waz/zclient/user/data/source/remote/UsersNetworkService.kt
+++ app/src/main/kotlin/com/waz/zclient/user/data/source/remote/UsersNetworkService.kt
@@ -2,7 +2,6 @@ package com.waz.zclient.user.data.source.remote
 
 import com.waz.zclient.user.data.source.remote.model.UserApi
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.GET
 import retrofit2.http.PUT
@@ -14,14 +13,14 @@ interface UsersNetworkService {
     suspend fun profileDetails(): Response<UserApi>
 
     @PUT("/self")
-    suspend fun changeName(@Body userApi: UserApi): Response<Void>
+    suspend fun changeName(@Field("name") name: String): Response<Void>
 
     @PUT("/self/handle")
-    suspend fun changeHandle(@Field("handle") value: String): Response<Void>
+    suspend fun changeHandle(@Field("handle") handle: String): Response<Void>
 
     @PUT("/self/email")
-    suspend fun changeEmail(@Field("email") value: String): Response<Void>
+    suspend fun changeEmail(@Field("email") email: String): Response<Void>
 
     @PUT("/self/phone")
-    suspend fun changePhone(@Field("phone") value: String): Response<UserApi>
+    suspend fun changePhone(@Field("phone") phone: String): Response<Void>
 }
