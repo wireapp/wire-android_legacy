eb9860611cdc688ed37c9fac9f9d7511d070c32c
Maciej Gorywoda <makingthematrix@gmail.com>
2020-01-16 18:26:06 +0100

    Blocked user fragment (#2534)
    * BlockedUserFragment
    * Fix back press issues in new connect request and block screens

--- app/src/main/scala/com/waz/zclient/participants/fragments/BlockedUserFragment.scala
+++ app/src/main/scala/com/waz/zclient/participants/fragments/BlockedUserFragment.scala
@@ -2,7 +2,6 @@ package com.waz.zclient.participants.fragments
 
 import android.os.Bundle
 import com.waz.model.UserId
-import com.waz.threading.Threading
 import com.waz.utils.returning
 import com.waz.zclient.R
 import com.waz.zclient.conversation.ConversationController
@@ -11,14 +10,15 @@ import com.waz.zclient.pages.main.conversation.controller.IConversationScreenCon
 import com.waz.zclient.participants.UserRequester
 import com.waz.zclient.views.menus.{FooterMenu, FooterMenuCallback}
 
-class SendConnectRequestFragment extends UntabbedRequestFragment {
-  import Threading.Implicits.Ui
+class BlockedUserFragment extends UntabbedRequestFragment {
 
-  override protected val Tag: String = SendConnectRequestFragment.Tag
+  import com.waz.threading.Threading.Implicits.Ui
+
+  override protected val Tag: String = BlockedUserFragment.Tag
 
   override protected lazy val footerCallback = new FooterMenuCallback {
     override def onLeftActionClicked(): Unit =
-      inject[UsersController].connectToUser(userToConnectId).foreach(_.foreach { _ => getActivity.onBackPressed() })
+      inject[UsersController].unblockUser(userToConnectId).foreach(_ => getActivity.onBackPressed())
 
     override def onRightActionClicked(): Unit =
       for {
@@ -31,8 +31,8 @@ class SendConnectRequestFragment extends UntabbedRequestFragment {
 
   override protected lazy val footerMenu = returning( view[FooterMenu](R.id.not_tabbed_footer) ) { vh =>
     vh.foreach { menu =>
-      menu.setLeftActionText(getString(R.string.glyph__plus))
-      menu.setLeftActionLabelText(getString(R.string.send_connect_request__connect_button__text))
+      menu.setLeftActionText(getString(R.string.glyph__block))
+      menu.setLeftActionLabelText(getString(R.string.connect_request__unblock__button__text))
       menu.setCallback(footerCallback)
     }
 
@@ -44,11 +44,11 @@ class SendConnectRequestFragment extends UntabbedRequestFragment {
   }
 }
 
-object SendConnectRequestFragment {
-  val Tag: String = classOf[SendConnectRequestFragment].getName
+object BlockedUserFragment {
+  val Tag: String = classOf[BlockedUserFragment].getName
 
-  def newInstance(userId: UserId, userRequester: UserRequester): SendConnectRequestFragment =
-    returning(new SendConnectRequestFragment)(fragment =>
+  def newInstance(userId: UserId, userRequester: UserRequester): BlockedUserFragment =
+    returning(new BlockedUserFragment)(fragment =>
       fragment.setArguments(returning(new Bundle) { args =>
         args.putString(UntabbedRequestFragment.ArgumentUserId, userId.str)
         args.putString(UntabbedRequestFragment.ArgumentUserRequester, userRequester.toString)
