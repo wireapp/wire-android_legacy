27f982d2b8eac722cb10b0d783db766562a0ddf8
Gizem Burhanoglu <gizembrh@gmail.com>
2020-01-20 16:14:06 +0100

    Update migration
    - Wrap it into KOTLIN_SETTINGS_MIGRATION condition
    - Clean magic numbers
    - Change naming

---
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/accountdata/ActiveAccountsMigration.kt
@@ -0,0 +1,36 @@
+package com.waz.zclient.storage.db.accountdata
+
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.waz.zclient.storage.BuildConfig
+
+private const val START_VERSION = 24
+private const val END_VERSION = 25
+
+val ACTIVE_ACCOUNTS_MIGRATION = object : Migration(START_VERSION, END_VERSION) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        if (BuildConfig.KOTLIN_SETTINGS_MIGRATION) {
+            val tempTableName = "ActiveAccountsTemp"
+            val createTempTable = """
+            CREATE TABLE IF NOT EXISTS $tempTableName (
+            `_id` TEXT PRIMARY KEY NOT NULL, 
+            `team_id` TEXT, 
+            `cookie` TEXT NOT NULL, 
+            `access_token` TEXT, 
+            `registered_push` TEXT,
+            `sso_id` TEXT)
+            """.trimIndent()
+            val deleteNullValues = "DELETE FROM ActiveAccounts WHERE _id IS NULL"
+            val copyAll = "INSERT INTO $tempTableName SELECT * FROM ActiveAccounts"
+            val dropOldTable = "DROP TABLE ActiveAccounts"
+            val renameTableBack = "ALTER TABLE $tempTableName RENAME TO ActiveAccounts"
+            with(database) {
+                execSQL(createTempTable)
+                execSQL(deleteNullValues)
+                execSQL(copyAll)
+                execSQL(dropOldTable)
+                execSQL(renameTableBack)
+            }
+        }
+    }
+}
--- storage/src/main/kotlin/com/waz/zclient/storage/di/StorageModule.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/di/StorageModule.kt
@@ -3,7 +3,7 @@ package com.waz.zclient.storage.di
 import androidx.room.Room
 import com.waz.zclient.storage.db.GlobalDatabase
 import com.waz.zclient.storage.db.UserDatabase
-import com.waz.zclient.storage.db.accountdata.GLOBAL_MIGRATION_24_25
+import com.waz.zclient.storage.db.accountdata.ACTIVE_ACCOUNTS_MIGRATION
 import com.waz.zclient.storage.db.users.migration.UserDatabaseMigration
 import com.waz.zclient.storage.pref.GlobalPreferences
 import com.waz.zclient.storage.pref.UserPreferences
@@ -23,7 +23,7 @@ val storageModule: Module = module {
             androidContext(),
             GlobalDatabase::class.java,
             GlobalDatabase.DB_NAME
-        ).addMigrations(GLOBAL_MIGRATION_24_25).build()
+        ).addMigrations(ACTIVE_ACCOUNTS_MIGRATION).build()
     }
     single { UserPreferences(androidContext(), get()) }
     single {}
