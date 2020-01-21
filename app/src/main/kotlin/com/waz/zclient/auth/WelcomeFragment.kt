7ca5dab6e554f003cf84c226c060c171ebb30384
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-01-21 15:47:39 +0100

    implement welcome screen ui

---
+++ app/src/main/kotlin/com/waz/zclient/auth/WelcomeFragment.kt
@@ -0,0 +1,25 @@
+package com.waz.zclient.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.waz.zclient.R
+
+
+class WelcomeFragment : Fragment(){
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    companion object {
+        fun newInstance() = WelcomeFragment()
+    }
+}
