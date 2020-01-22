2fb1c80b03c4c1dbec52f29a42538c5619cfeeef
BradleyWire <bradley.wilson@wire.com>
2020-01-22 14:12:34 +0100

    added wire headers to new classes

--- app/src/main/kotlin/com/waz/zclient/core/permissions/requesting/PermissionRequesters.kt
+++ app/src/main/kotlin/com/waz/zclient/core/permissions/requesting/PermissionRequesters.kt
@@ -1,3 +1,20 @@
+/**
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.waz.zclient.core.permissions.requesting
 
 import android.app.Activity
@@ -17,7 +34,7 @@ class PostMarshmallowPermissionRequest(val context: WeakReference<Activity>) : P
 
 class PreMarshmallowPermissionRequest : PermissionRequester {
     override fun invoke(p1: Array<out String>, p2: Int) {
-        // Pre marshmallow will behaves using the manifest
+        // Pre marshmallow behaves using the manifest permissions
     }
 }
 
