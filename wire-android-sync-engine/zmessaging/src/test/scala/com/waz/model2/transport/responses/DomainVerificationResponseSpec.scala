3da61ae92273c1974cece5768e9d600d0d342933
Gizem Burhanoglu <gizembrh@gmail.com>
2020-01-30 17:04:00 +0100

    Fix "No tests found" error for DomainVerificationResponseSpec.
    - Add error message for invalid input on SSO login

--- wire-android-sync-engine/zmessaging/src/test/scala/com/waz/model2/transport/responses/DomainVerificationResponseSpec.scala
+++ wire-android-sync-engine/zmessaging/src/test/scala/com/waz/model2/transport/responses/DomainVerificationResponseSpec.scala
@@ -1,39 +1,37 @@
 package com.waz.model2.transport.responses
 
-import com.waz.specs.ZSpec
 import org.json.JSONObject
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import org.scalatest.RobolectricTests
 
 @RunWith(classOf[RobolectricTestRunner])
-class DomainVerificationResponseSpec extends ZSpec with RobolectricTests {
+class DomainVerificationResponseSpec {
 
-  feature("Response parsing") {
-    scenario("given response has config_json_url responseDecoder returns DomainSuccessful with given url") {
-
-      val configUrl = "https://wire-rest.https.orange.com/config.json"
-      val jsonString =
-        s"""
+  @Test
+  def `given response has config_json_url responseDecoder returns DomainSuccessful with given url`() {
+    val configUrl = "https://wire-rest.https.orange.com/config.json"
+    val jsonString =
+      s"""
           |{
           |  "config_json_url": "$configUrl",
           |  "webapp_welcome_url": "https://app.wire.orange.com/"
           |}
         """.stripMargin
-      val jsonObject = new JSONObject(jsonString)
+    val jsonObject = new JSONObject(jsonString)
 
-      val response = DomainVerificationResponse.DomainVerificationResponseDecoder.apply(jsonObject)
+    val response = DomainVerificationResponse.DomainVerificationResponseDecoder.apply(jsonObject)
 
-      assert(response == DomainSuccessful(configUrl))
-    }
+    assert(response == DomainSuccessful(configUrl))
+  }
 
-    scenario("given response has no config_json_url, responseDecoder returns DomainNotFound type") {
-      val jsonObject = new JSONObject("{}")
+  @Test
+  def `given response has no config_json_url, responseDecoder returns DomainNotFound type`() {
+    val jsonObject = new JSONObject("{}")
 
-      val response = DomainVerificationResponse.DomainVerificationResponseDecoder.apply(jsonObject)
+    val response = DomainVerificationResponse.DomainVerificationResponseDecoder.apply(jsonObject)
 
-      assert(response == DomainNotFound)
-    }
+    assert(response == DomainNotFound)
   }
 
 }
