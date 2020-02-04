234fcbc17fd37ff295ae9f24dea30c5938a7fc8e
BradleyWire <bradley.wilson@wire.com>
2020-02-04 16:33:36 +0100

    detekt issues

--- app/src/main/kotlin/com/waz/zclient/core/extension/Fragment.kt
+++ app/src/main/kotlin/com/waz/zclient/core/extension/Fragment.kt
@@ -3,7 +3,6 @@ package com.waz.zclient.core.extension
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
