955cc9e99ae07d00ac37524f4a157b80c018fff2
Gizem Burhanoglu <gizembrh@gmail.com>
2020-02-13 17:22:26 +0100

    Fix missing migrations reference

--- storage/src/main/kotlin/com/waz/zclient/storage/di/StorageModule.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/di/StorageModule.kt
@@ -3,8 +3,6 @@ package com.waz.zclient.storage.di
 import androidx.room.Room
 import com.waz.zclient.storage.db.GlobalDatabase
 import com.waz.zclient.storage.db.UserDatabase
-import com.waz.zclient.storage.db.accountdata.ACTIVE_ACCOUNTS_MIGRATION
-import com.waz.zclient.storage.db.users.migration.USER_DATABASE_MIGRATION_126_TO_127
 import com.waz.zclient.storage.pref.GlobalPreferences
 import com.waz.zclient.storage.pref.UserPreferences
 import org.koin.android.ext.koin.androidContext
@@ -17,14 +15,14 @@ val storageModule: Module = module {
         Room.databaseBuilder(androidContext(),
             UserDatabase::class.java,
             get<GlobalPreferences>().activeUserId
-        ).addMigrations(USER_DATABASE_MIGRATION_126_TO_127).build()
+        ).addMigrations(*UserDatabase.migrations).build()
     }
     single {
         Room.databaseBuilder(
             androidContext(),
             GlobalDatabase::class.java,
             GlobalDatabase.DB_NAME
-        ).addMigrations(ACTIVE_ACCOUNTS_MIGRATION).build()
+        ).addMigrations(*GlobalDatabase.migrations).build()
     }
     single { UserPreferences(androidContext(), get()) }
 }
