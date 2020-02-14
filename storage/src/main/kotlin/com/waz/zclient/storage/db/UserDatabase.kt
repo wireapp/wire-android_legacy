608a0b6e41d2f33d0895fdd473b3fb8d07ec6fa7
Gizem Burhanoglu <gizembrh@gmail.com>
2020-02-14 12:22:27 +0100

    Fix: Update room db version and fix migration versions (#2614)
    * Update room db version and fix migration versions
    
    * Add unit test for room db version checks
    
    * Fix missing migrations reference
    
    * Delete redundant maven repository
    
    - mavenCentral() repository is the new version of repo1.maven.org, so we don't need both.
    
    * Suppress detekt's SpreadOperator warning since the performance overhead is negligible

--- storage/src/main/kotlin/com/waz/zclient/storage/db/UserDatabase.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/UserDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.waz.zclient.storage.db.clients.model.ClientEntity
 import com.waz.zclient.storage.db.clients.service.ClientsDao
+import com.waz.zclient.storage.db.users.migration.USER_DATABASE_MIGRATION_126_TO_127
 import com.waz.zclient.storage.db.users.model.UserEntity
 import com.waz.zclient.storage.db.users.model.UserPreferenceEntity
 import com.waz.zclient.storage.db.users.service.UserDao
@@ -11,7 +12,7 @@ import com.waz.zclient.storage.db.users.service.UserPreferenceDao
 
 @Database(
     entities = [UserPreferenceEntity::class, UserEntity::class, ClientEntity::class],
-    version = 126,
+    version = UserDatabase.VERSION,
     exportSchema = false
 )
 abstract class UserDatabase : RoomDatabase() {
@@ -19,4 +20,11 @@ abstract class UserDatabase : RoomDatabase() {
     abstract fun userPreferencesDbService(): UserPreferenceDao
     abstract fun userDbService(): UserDao
     abstract fun clientsDbService(): ClientsDao
+
+    companion object {
+        const val VERSION = 127
+
+        @JvmStatic
+        val migrations = arrayOf(USER_DATABASE_MIGRATION_126_TO_127)
+    }
 }
--- storage/src/main/kotlin/com/waz/zclient/storage/di/StorageModule.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/di/StorageModule.kt
@@ -3,8 +3,6 @@ package com.waz.zclient.storage.di
 import androidx.room.Room
 import com.waz.zclient.storage.db.GlobalDatabase
 import com.waz.zclient.storage.db.UserDatabase
-import com.waz.zclient.storage.db.accountdata.ACTIVE_ACCOUNTS_MIGRATION
-import com.waz.zclient.storage.db.users.migration.USER_DATABASE_MIGRATION_126_TO_127
 import com.waz.zclient.storage.pref.GlobalPreferences
 import com.waz.zclient.storage.pref.UserPreferences
 import org.koin.android.ext.koin.androidContext
@@ -14,17 +12,19 @@ import org.koin.dsl.module
 val storageModule: Module = module {
     single { GlobalPreferences(androidContext()) }
     single {
+        @Suppress("SpreadOperator")
         Room.databaseBuilder(androidContext(),
             UserDatabase::class.java,
             get<GlobalPreferences>().activeUserId
-        ).addMigrations(USER_DATABASE_MIGRATION_126_TO_127).build()
+        ).addMigrations(*UserDatabase.migrations).build()
     }
     single {
+        @Suppress("SpreadOperator")
         Room.databaseBuilder(
             androidContext(),
             GlobalDatabase::class.java,
             GlobalDatabase.DB_NAME
-        ).addMigrations(ACTIVE_ACCOUNTS_MIGRATION).build()
+        ).addMigrations(*GlobalDatabase.migrations).build()
     }
     single { UserPreferences(androidContext(), get()) }
 }
