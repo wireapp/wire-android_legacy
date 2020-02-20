59c98f53f85b63a92154ce543039589dc2e2cfea
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-02-20 18:14:32 +0100

    chore: bump material version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -16,7 +16,7 @@ object Versions {
     //build
     const val COROUTINES = "1.3.2"
     const val WORK_MANAGER = "2.0.1"
-    const val ANDROIDX_MATERIAL = "1.0.0"
+    const val ANDROIDX_MATERIAL = "1.1.0"
     const val ANDROIDX_MULTIDEX = "2.0.0"
     const val ANDROIDX_APP_COMPAT = "1.0.0"
     const val ANDROIDX_RECYCLER_VIEW = "1.0.0"
