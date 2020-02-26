6ae78238539d98d3828a45926e80a29a32f1804f
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-02-26 12:16:52 +0100

    feat: create personal account with email - prepare email verification screen

--- app/src/main/kotlin/com/waz/zclient/auth/registration/personal/email/CreatePersonalAccountEmailVerificationFragment.kt
+++ app/src/main/kotlin/com/waz/zclient/auth/registration/personal/email/CreatePersonalAccountEmailVerificationFragment.kt
@@ -1,17 +1,18 @@
-package com.waz.zclient.auth.registration.personal
+package com.waz.zclient.auth.registration.personal.email
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.waz.zclient.R
 
-class CreatePersonalAccountWithPhoneFragment : Fragment(R.layout.fragment_create_personal_account_with_phone) {
+class CreatePersonalAccountEmailVerificationFragment : Fragment(R.layout.fragment_create_personal_account_email_verification) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
     }
 
     companion object {
-        fun newInstance() = CreatePersonalAccountWithPhoneFragment()
+        fun newInstance() = CreatePersonalAccountEmailVerificationFragment()
     }
 }
--- app/src/main/kotlin/com/waz/zclient/auth/registration/personal/email/CreatePersonalAccountWithEmailFragment.kt
+++ app/src/main/kotlin/com/waz/zclient/auth/registration/personal/email/CreatePersonalAccountWithEmailFragment.kt
@@ -1,11 +1,14 @@
-package com.waz.zclient.auth.registration.personal
+package com.waz.zclient.auth.registration.personal.email
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.observe
 import com.waz.zclient.R
+import com.waz.zclient.auth.registration.personal.CreatePersonalAccountViewModel
+import com.waz.zclient.core.extension.replaceFragment
 import kotlinx.android.synthetic.main.fragment_create_personal_account_with_email.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -17,7 +20,7 @@ class CreatePersonalAccountWithEmailFragment : Fragment(R.layout.fragment_create
         super.onViewCreated(view, savedInstanceState)
         initViewModel()
         initEmailChangedListener()
-        updateConfirmationButtonStatus(false)
+        initConfirmationButton()
     }
 
     private fun initViewModel() {
@@ -34,6 +37,17 @@ class CreatePersonalAccountWithEmailFragment : Fragment(R.layout.fragment_create
         createPersonalAccountWithEmailEditText.doAfterTextChanged {
             createPersonalAccountViewModel.validateEmail(it.toString())
         }
+    }
+
+    private fun initConfirmationButton() {
+        updateConfirmationButtonStatus(false)
+        confirmationButton.setOnClickListener {
+            replaceFragment(CreatePersonalAccountEmailVerificationFragment.newInstance())
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        (activity as AppCompatActivity).replaceFragment(R.id.activityCreateAccountLayoutContainer, fragment)
     }
 
     companion object {
