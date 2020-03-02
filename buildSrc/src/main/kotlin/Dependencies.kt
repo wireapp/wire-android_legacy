5928a5c417a084569caf429a38a5fb8714b67ec6
Gizem Burhanoglu <gizembrh@gmail.com>
2020-03-02 15:54:17 +0100

    Downgrade material library to fix bottom navigation background issue

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -16,7 +16,9 @@ object Versions {
     //build
     const val COROUTINES = "1.3.2"
     const val WORK_MANAGER = "2.0.1"
-    const val ANDROIDX_MATERIAL = "1.1.0"
+    //TODO: do not upgrade to 1.1.0 until google fixes this issue:
+    //https://github.com/material-components/material-components-android/issues/1077
+    const val ANDROIDX_MATERIAL = "1.0.0"
     const val ANDROIDX_MULTIDEX = "2.0.0"
     const val ANDROIDX_APP_COMPAT = "1.0.0"
     const val ANDROIDX_RECYCLER_VIEW = "1.0.0"
