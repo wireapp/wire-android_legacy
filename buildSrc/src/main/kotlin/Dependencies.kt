d6461f303c48efbd5c27ad2a10b7ca90a1d04cbc
Maciej Gorywoda <makingthematrix@gmail.com>
2020-03-03 13:14:00 +0100

    chore: Version bump to 3.46 (#2660)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     //wire android client
-    const val ANDROID_CLIENT_MAJOR_VERSION = "3.45."
+    const val ANDROID_CLIENT_MAJOR_VERSION = "3.46."
 
     //core
     const val KOTLIN = "1.3.60"
