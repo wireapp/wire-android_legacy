381232f0c524500c312cef1da24066480381e016
BradleyWire <57259490+BradleyWire@users.noreply.github.com>
2020-03-05 11:07:31 +0000

    AN-6714 && AN-6716: Conversation Search feature and about screen is broken  (#2663)
    Fixed AN-6714 and AN-6716: Where the conversation search feature was broken and the about screen was broken

--- app/src/main/kotlin/com/waz/zclient/settings/about/SettingsAboutActivity.kt
+++ app/src/main/kotlin/com/waz/zclient/settings/about/SettingsAboutActivity.kt
@@ -5,12 +5,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.waz.zclient.R
+import com.waz.zclient.core.extension.createScope
 import com.waz.zclient.core.extension.replaceFragment
+import com.waz.zclient.settings.di.SETTINGS_SCOPE
+import com.waz.zclient.settings.di.SETTINGS_SCOPE_ID
 import kotlinx.android.synthetic.main.activity_settings_about.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.InternalCoroutinesApi
 
 class SettingsAboutActivity : AppCompatActivity() {
+
+    private val scope = createScope(
+        scopeId = SETTINGS_SCOPE_ID,
+        scopeName = SETTINGS_SCOPE
+    )
 
     @ExperimentalCoroutinesApi
     @InternalCoroutinesApi
@@ -30,5 +38,10 @@ class SettingsAboutActivity : AppCompatActivity() {
     companion object {
         @JvmStatic
         fun newIntent(context: Context) = Intent(context, SettingsAboutActivity::class.java)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        scope.close()
     }
 }
