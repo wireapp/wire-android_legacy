f22c911877b2aee79efe653bf34e7442a7bd7329
Gizem Burhanoglu <gizembrh@gmail.com>
2020-03-05 17:07:15 +0100

    Fix Fragment.openUrl method deprecation message

--- app/src/main/kotlin/com/waz/zclient/core/extension/Fragment.kt
+++ app/src/main/kotlin/com/waz/zclient/core/extension/Fragment.kt
@@ -16,7 +16,7 @@ inline fun <T : Fragment> T.withArgs(argsBuilder: Bundle.() -> Unit): T =
         arguments = Bundle().apply(argsBuilder)
     }
 
-@Deprecated("use Navigator#openUri", ReplaceWith("navigator().openUri(Uri)"))
+@Deprecated("use Navigator#navigateTo(Uri)", ReplaceWith("navigator().navigateTo(uri)"))
 fun Fragment.openUrl(url: String) =
     requireActivity().startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
 
