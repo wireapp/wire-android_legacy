d14e0da5e7625a15b019cdea54698fdaf09e315e
Gizem Burhanoglu <gizembrh@gmail.com>
2020-03-12 14:44:29 +0100

    Feat: Buttons - Part 2: Add layouts for Composite view buttons (#2675)
    feat: Add layouts for Composite & its buttons, bind ButtonData signal to button ui layout

---
+++ app/src/main/scala/com/waz/zclient/messages/parts/composite/ButtonContainerView.scala
@@ -0,0 +1,48 @@
+package com.waz.zclient.messages.parts.composite
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import com.waz.model.{ButtonData, ButtonId, MessageId}
+import com.waz.utils.events.{EventStream, Signal, Subscription}
+import com.waz.zclient.{R, ViewHelper}
+
+class ButtonContainerView(context: Context, attrs: AttributeSet, style: Int)
+    extends LinearLayout(context, attrs, style)
+    with ViewHelper {
+
+  def this(context: Context, attrs: AttributeSet) = this(context, attrs, 0)
+  def this(context: Context) = this(context, null)
+
+  setOrientation(LinearLayout.VERTICAL)
+
+  //TODO: observe this and send ButtonAction request
+  val selectedButtonId = EventStream[(MessageId, ButtonId)]()
+
+  val buttonsSignal = Signal[Seq[ButtonData]]()
+
+  private var subscriptions = Set.empty[Subscription]
+
+  //we already receive ordered
+  buttonsSignal.onUi { setButtons }
+
+  //TODO: convert to a more efficient way. calculate diff.
+  private def setButtons(items: Seq[ButtonData]): Unit = {
+    removeAllViews()
+    clearSubscriptions()
+
+    items.foreach { data =>
+      val buttonItemView =
+        inflate(R.layout.composite_message_alarm_buttonitemview, this, addToParent = false)
+          .asInstanceOf[ButtonItemView]
+      buttonItemView.bindButton(ButtonItemViewUIModel(data.title, data.state))
+      subscriptions += buttonItemView.selected.onUi(_ => selectedButtonId ! data.id)
+      addView(buttonItemView)
+    }
+  }
+
+  private def clearSubscriptions(): Unit = {
+    subscriptions.foreach(_.unsubscribe())
+    subscriptions = Set.empty[Subscription]
+  }
+}
