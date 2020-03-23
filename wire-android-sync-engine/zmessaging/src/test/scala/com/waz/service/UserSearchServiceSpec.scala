aeef3e30c9b498e30088c2d6012759dbbbfc9e60
Gizem Burhanoglu <gizembrh@gmail.com>
2020-03-23 15:14:52 +0100

    Add unit tests for e-mail search case

--- wire-android-sync-engine/zmessaging/src/test/scala/com/waz/service/UserSearchServiceSpec.scala
+++ wire-android-sync-engine/zmessaging/src/test/scala/com/waz/service/UserSearchServiceSpec.scala
@@ -124,6 +124,13 @@ class UserSearchServiceSpec extends AndroidFreeSpec with DerivedLogTag {
       permissions = (adminPermissions, adminPermissions),
       teamId = teamId,
       handle = Some(Handle("aa2"))
+    ),
+    id('me1) -> UserData(id('me1), "Team Member With Email").copy(
+      email = Some(EmailAddress("a_member@wire.com")),
+      teamId = teamId
+    ),
+    id('pe1) -> UserData(id('pe1), "Person With Email").copy(
+      email = Some(EmailAddress("a_person@wire.com"))
     )
   )
 
@@ -245,6 +252,14 @@ class UserSearchServiceSpec extends AndroidFreeSpec with DerivedLogTag {
 
     scenario("Return no search results for handle") {
       verifySearch("@relt", Set.empty[UserId])
+    }
+
+    scenario("Return no search results for team member when query matches only to email") {
+      verifySearch("a_member@wire.com", Set.empty[UserId])
+    }
+
+    scenario("Return no search results for personal account when query matches only to email") {
+      verifySearch("a_person@wire.com", Set.empty[UserId])
     }
 
   }
