1250eab7d538ffe2ff255e18e57d65ed8c335a59
Gizem Burhanoglu <gizembrh@gmail.com>
2020-03-23 16:35:34 +0100

    Settings Background - Part 1: Add AssetsRepository and related classes (#2709)
    * Add AssetsRepository and related classes
    
    * Extract AssetsRepository to an interface

---
+++ app/src/main/kotlin/com/waz/zclient/assets/di/AssetsModule.kt
@@ -0,0 +1,17 @@
+package com.waz.zclient.assets.di
+
+import com.waz.zclient.assets.AssetsApi
+import com.waz.zclient.assets.AssetsRepository
+import com.waz.zclient.assets.datasources.AssetsDataSource
+import com.waz.zclient.assets.datasources.AssetsRemoteDataSource
+import com.waz.zclient.assets.mapper.AssetMapper
+import com.waz.zclient.core.network.NetworkClient
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val assetsModule: Module = module {
+    factory { AssetMapper() }
+    single { AssetsDataSource(get(), get()) as AssetsRepository }
+    factory { AssetsRemoteDataSource(get(), get()) }
+    factory { get<NetworkClient>().create(AssetsApi::class.java) }
+}
