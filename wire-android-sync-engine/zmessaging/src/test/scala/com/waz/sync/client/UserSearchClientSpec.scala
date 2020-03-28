14255501590f598c85221dc17a0ee12cd329a5a9
John Nguyen <polyxo@protonmail.com>
2020-03-28 16:38:36 +0100

    Use new exact handle search endpoint

--- wire-android-sync-engine/zmessaging/src/test/scala/com/waz/sync/client/UserSearchClientSpec.scala
+++ wire-android-sync-engine/zmessaging/src/test/scala/com/waz/sync/client/UserSearchClientSpec.scala
@@ -1,7 +1,8 @@
 package com.waz.sync.client
 
 import com.waz.specs.AndroidFreeSpec
-import com.waz.sync.client.UserSearchClient.{ExactHandleResponse, UserSearchResponse}
+import com.waz.sync.client.UserSearchClient._
+import UserSearchResponse._
 import com.waz.utils.CirceJSONSupport
 
 class UserSearchClientSpec extends AndroidFreeSpec with CirceJSONSupport {
@@ -9,6 +10,7 @@ class UserSearchClientSpec extends AndroidFreeSpec with CirceJSONSupport {
   feature("User search response decoding") {
 
     import io.circe.parser._
+
 
     scenario("Contact response decoding") {
       // Given
@@ -38,17 +40,23 @@ class UserSearchClientSpec extends AndroidFreeSpec with CirceJSONSupport {
       val result = decode[UserSearchResponse](response)
 
       // Then
-      val user1 = UserSearchResponse.User(id = "d9700541-9b05-47b5-b85f-4a195593af71",
-                                          name = "ma",
-                                          handle = Some("ma75"),
-                                          accent_id = None,
-                                          team_id = Some("399a5fd1-9a2b-4339-a005-09518baba91b"))
+      val user1 = User(
+        id = "d9700541-9b05-47b5-b85f-4a195593af71",
+        name = "ma",
+        handle = Some("ma75"),
+        accent_id = None,
+        team_id = Some("399a5fd1-9a2b-4339-a005-09518baba91b"),
+        assets = Seq.empty
+      )
 
-      val user2 = UserSearchResponse.User(id = "b8864084-454c-4458-8e81-caf3a32780a7",
-                                          name = "MA",
-                                          handle = None,
-                                          accent_id = Some(0),
-                                          team_id = None)
+      val user2 = User(
+        id = "b8864084-454c-4458-8e81-caf3a32780a7",
+        name = "MA",
+        handle = None,
+        accent_id = Some(0),
+        team_id = None,
+        assets = Seq.empty
+      )
 
       val documents = Seq(user1, user2)
 
@@ -69,11 +77,6 @@ class UserSearchClientSpec extends AndroidFreeSpec with CirceJSONSupport {
           |      "key": "3-2-78d7e7a8-357b-47bd-be69-c3be68056c9b",
           |      "size": "preview",
           |      "type": "image"
-          |    },
-          |    {
-          |      "key": "3-2-5c32d3e7-66d5-455c-817d-5c33febacf72",
-          |      "size": "complete",
-          |      "type": "image"
           |    }
           |  ],
           |  "team": "399a5fd1-9a2b-4339-a005-09518baba91b",
@@ -84,13 +87,28 @@ class UserSearchClientSpec extends AndroidFreeSpec with CirceJSONSupport {
         """.stripMargin
 
       // When
-      val result = decode[ExactHandleResponse](response)
+      val result = decode[User](response)
 
       // Then
+      val asset = Asset(
+        key = "3-2-78d7e7a8-357b-47bd-be69-c3be68056c9b",
+        size = "preview",
+        `type` = "image"
+      )
+
+      val user = User(
+        id = "d3410a8f-8903-4e2c-93e3-4fa039410e4f",
+        name = "aaa",
+        handle = Some("aaa"),
+        accent_id = Some(2),
+        team_id = Some("399a5fd1-9a2b-4339-a005-09518baba91b"),
+        assets = Seq(asset)
+      )
+
       result.isRight shouldBe true
 
       val searchResult = result.right.get
-      searchResult shouldEqual ExactHandleResponse(id = "d3410a8f-8903-4e2c-93e3-4fa039410e4f")
+      searchResult shouldEqual user
     }
 
   }
