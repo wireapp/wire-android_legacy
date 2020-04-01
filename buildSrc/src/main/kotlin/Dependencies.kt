d8d561d4ee7120768d2a0875a4f6c95d4ee6091c
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-04-01 11:25:13 +0200

    build : bump version to 3.47

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     //wire android client
-    const val ANDROID_CLIENT_MAJOR_VERSION = "3.46."
+    const val ANDROID_CLIENT_MAJOR_VERSION = "3.47."
 
     //core
     const val KOTLIN = "1.3.60"
