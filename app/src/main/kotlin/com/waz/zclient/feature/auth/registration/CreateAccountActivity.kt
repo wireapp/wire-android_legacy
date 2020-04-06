5fd3b8369142c649f2585460e54cb319870bd625
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-04-06 18:57:11 +0200

    feat: create personal account with email : add back button

--- app/src/main/kotlin/com/waz/zclient/feature/auth/registration/CreateAccountActivity.kt
+++ app/src/main/kotlin/com/waz/zclient/feature/auth/registration/CreateAccountActivity.kt
@@ -9,6 +9,7 @@ import com.waz.zclient.core.extension.createScope
 import com.waz.zclient.core.extension.replaceFragment
 import com.waz.zclient.feature.auth.registration.di.REGISTRATION_SCOPE
 import com.waz.zclient.feature.auth.registration.di.REGISTRATION_SCOPE_ID
+import kotlinx.android.synthetic.main.activity_create_account.*
 
 class CreateAccountActivity : AppCompatActivity(R.layout.activity_create_account) {
 
@@ -20,11 +21,17 @@ class CreateAccountActivity : AppCompatActivity(R.layout.activity_create_account
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         replaceFragment(R.id.activityCreateAccountLayoutContainer, CreateAccountFragment.newInstance(), false)
+        initBackButton()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         scope.close()
+    }
+
+    private fun initBackButton() {
+        activityCreateAccountBackButton.bringToFront()
+        activityCreateAccountBackButton.setOnClickListener { onBackPressed() }
     }
 
     companion object {
