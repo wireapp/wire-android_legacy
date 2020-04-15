07ab190f5d3de519c90dd1cf1cba4134eae35af7
Gizem Burhanoglu <gizembrh@gmail.com>
2020-04-15 14:42:27 +0200

    Fix NPE in AccessTokenEntity conversion

--- storage/src/main/kotlin/com/waz/zclient/storage/db/accountdata/AccessTokenEntity.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/accountdata/AccessTokenEntity.kt
@@ -12,9 +12,9 @@ data class AccessTokenEntity(
 
 class AccessTokenConverter {
     @TypeConverter
-    fun fromStringToAccessToken(tokenString: String): AccessTokenEntity? =
+    fun fromStringToAccessToken(tokenString: String?): AccessTokenEntity? = tokenString?.let {
         try {
-            val json = JSONObject(tokenString)
+            val json = JSONObject(it)
             AccessTokenEntity(
                 token = json.getString(KEY_TOKEN),
                 tokenType = json.getString(KEY_TOKEN_TYPE),
@@ -23,6 +23,7 @@ class AccessTokenConverter {
         } catch (e: JSONException) {
             null
         }
+    }
 
     @TypeConverter
     fun accessTokenToString(entity: AccessTokenEntity): String =
