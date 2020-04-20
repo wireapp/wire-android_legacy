a5b63a4942c6015cedbf5cf5e295ff1eb64cf52d
Maciej Gorywoda <makingthematrix@gmail.com>
2020-04-20 19:09:08 +0200

    fix: Global database changes moved to a new migration (#2780)
    This is similar to what we did with the user database: all changes since the 3.47 version with the bug in migrations
    were moved to a new migration, so that people with the faulty 3.47 may have a chance for a fix.
    Please test by both going back to 3.46 and updating to this one, and by going back to the faulty 3.47 and updating.
    
    There's also one small change to the Buttons table migration as I ran into a problem with it while working on this fix.
    It's possible that the Buttons table schema created by the faulty 3.47 is different than what we have now.
    It can be fixed by simply deleting the old table - Buttons is a new feature and the table should be empty.

--- storage/src/main/kotlin/com/waz/zclient/storage/db/GlobalDatabase.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/GlobalDatabase.kt
@@ -5,8 +5,9 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.waz.zclient.storage.db.accountdata.AccessTokenConverter
 import com.waz.zclient.storage.db.accountdata.ActiveAccountsDao
-import com.waz.zclient.storage.db.accountdata.ActiveAccountsEntity
 import com.waz.zclient.storage.db.accountdata.GLOBAL_DATABASE_MIGRATION_24_25
+import com.waz.zclient.storage.db.accountdata.GLOBAL_DATABASE_MIGRATION_25_26
+import com.waz.zclient.storage.db.accountdata.ActiveAccountsEntity
 import com.waz.zclient.storage.db.accountdata.SsoIdConverter
 import com.waz.zclient.storage.db.cache.CacheEntryDao
 import com.waz.zclient.storage.db.cache.CacheEntryEntity
@@ -27,9 +28,9 @@ abstract class GlobalDatabase : RoomDatabase() {
 
     companion object {
         const val DB_NAME = "ZGlobal.db"
-        const val VERSION = 25
+        const val VERSION = 26
 
         @JvmStatic
-        val migrations = arrayOf(GLOBAL_DATABASE_MIGRATION_24_25)
+        val migrations = arrayOf(GLOBAL_DATABASE_MIGRATION_24_25, GLOBAL_DATABASE_MIGRATION_25_26)
     }
 }
