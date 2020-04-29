372ffbcb31bb20eb05beb8cb6d9391b3e056d2cc
Gizem Burhanoglu <gizembrh@gmail.com>
2020-04-29 14:01:47 +0200

    Fix app crash on Android 5 and 6 devices (#2805)

--- app/src/main/kotlin/com/waz/zclient/core/images/transformations/DarkenTransformation.kt
+++ app/src/main/kotlin/com/waz/zclient/core/images/transformations/DarkenTransformation.kt
@@ -9,6 +9,7 @@ import android.graphics.Paint
 import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import java.security.MessageDigest
+import java.util.Objects
 
 data class DarkenTransformation(
     private val alphaValue: Int,
@@ -42,4 +43,12 @@ data class DarkenTransformation(
 
         return bitmap
     }
+
+    override fun hashCode(): Int = Objects.hash(alphaValue, saturation)
+
+    override fun equals(other: Any?): Boolean =
+        other === this ||
+            (other is DarkenTransformation &&
+                other.alphaValue == this.alphaValue &&
+                other.saturation == this.saturation)
 }
