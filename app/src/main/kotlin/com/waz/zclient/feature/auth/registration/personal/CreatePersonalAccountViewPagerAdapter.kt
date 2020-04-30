5c5a9eee9d3dbb0912747f654e194581e7b91a70
Mejdeddine Benzarti <43655743+mejdoo@users.noreply.github.com>
2020-04-30 12:56:38 +0200

    Create personal account part 13 (#2801)
    - Handle keyboard visibility through different screens.
    - Rollback to viewPager instead of viewPager2

--- app/src/main/kotlin/com/waz/zclient/feature/auth/registration/personal/CreatePersonalAccountViewPagerAdapter.kt
+++ app/src/main/kotlin/com/waz/zclient/feature/auth/registration/personal/CreatePersonalAccountViewPagerAdapter.kt
@@ -1,17 +1,17 @@
 package com.waz.zclient.feature.auth.registration.personal
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import com.waz.zclient.feature.auth.registration.personal.email.CreatePersonalAccountEmailFragment
 import com.waz.zclient.feature.auth.registration.personal.phone.CreatePersonalAccountPhoneFragment
 
-class CreatePersonalAccountViewPagerAdapter(fragmentActivity: FragmentActivity) :
-    FragmentStateAdapter(fragmentActivity) {
+class CreatePersonalAccountViewPagerAdapter(fragmentManager: FragmentManager, private val titles: List<String>) :
+    FragmentPagerAdapter(fragmentManager) {
 
-    override fun getItemCount(): Int = TAB_SIZE
+    override fun getCount(): Int = titles.size
 
-    override fun createFragment(position: Int): Fragment {
+    override fun getItem(position: Int): Fragment {
         var fragment = Fragment()
         when (position) {
             EMAIL_TAB_POSITION -> fragment = CreatePersonalAccountEmailFragment.newInstance()
@@ -20,9 +20,10 @@ class CreatePersonalAccountViewPagerAdapter(fragmentActivity: FragmentActivity) 
         return fragment
     }
 
+    override fun getPageTitle(position: Int): CharSequence = titles[position].toUpperCase()
+
     companion object {
         private const val EMAIL_TAB_POSITION = 0
         private const val PHONE_TAB_POSITION = 1
-        private const val TAB_SIZE = 2
     }
 }
