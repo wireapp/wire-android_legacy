779ca3f437ce7ede5e6af92a6a9ce5bd607412b2
Maciej Gorywoda <makingthematrix@gmail.com>
2020-05-05 17:42:15 +0200

    fix: Migrate data only if the original table exists

--- storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/MigrationUtils.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/MigrationUtils.kt
@@ -3,6 +3,18 @@ package com.waz.zclient.storage.db.users.migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
 object MigrationUtils {
+    private fun tableExists(database: SupportSQLiteDatabase, tableName: String): Boolean {
+        val query = "SELECT DISTINCT tbl_name FROM sqlite_master WHERE tbl_name = '$tableName'"
+        database.query(query).use { cursor ->
+            if (cursor != null) {
+                if (cursor.count > 0) {
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
     fun executeSimpleMigration(
         database: SupportSQLiteDatabase,
         originalTableName: String,
@@ -17,8 +29,10 @@ object MigrationUtils {
         with(database) {
             execSQL(dropTempTableIfExists)
             execSQL(createTempTable)
-            execSQL(copyAll)
-            execSQL(dropOldTable)
+            if (tableExists(database, originalTableName)) {
+                execSQL(copyAll)
+                execSQL(dropOldTable)
+            }
             execSQL(renameTableBack)
             indicesCalls.forEach {
                 execSQL(it)
