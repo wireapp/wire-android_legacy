fa3730d7466e9b36245666bf7be60801b9ac1b31
Mejdeddine Benzarti <43655743+mejdoo@users.noreply.github.com>
2020-05-11 16:38:19 +0200

    Feature - OnBoarding Flow tweaks - part 1
    implement an informative screen when moving to custom backend

--- app/src/main/scala/com/waz/zclient/appentry/StartSSOFragment.scala
+++ app/src/main/scala/com/waz/zclient/appentry/StartSSOFragment.scala
@@ -1,13 +1,15 @@
 package com.waz.zclient.appentry
 
-import android.os.Bundle
+import android.os.{Bundle, CountDownTimer}
 import android.view.{LayoutInflater, View, ViewGroup}
+import android.widget.{ProgressBar, TextView}
 import com.waz.api.impl.ErrorResponse
 import com.waz.api.impl.ErrorResponse.{ConnectionErrorCode, TimeoutCode}
 import com.waz.model2.transport.responses.SSOFound
 import com.waz.threading.Threading
 import com.waz.zclient.R
 import com.waz.zclient.appentry.DialogErrorMessage.GenericDialogErrorMessage
+import com.waz.zclient.utils.BackendController
 import com.waz.zclient.utils.ContextUtils.showErrorDialog
 
 import scala.concurrent.Future
@@ -16,12 +18,31 @@ class StartSSOFragment extends SSOFragment {
 
   import com.waz.threading.Threading.Implicits.Ui
 
+  private val TIME_TO_WAIT: Long = 5000
+  private val COUNTDOWN_INTERVAL: Long = 1000
+  private lazy val progressBar = view[ProgressBar](R.id.startSsoProgressBar)
+  private lazy val linkTextView = view[TextView](R.id.startSsoLinkTextView)
+  private lazy val backendController = inject[BackendController]
+
+  private lazy val timer = new CountDownTimer(TIME_TO_WAIT, COUNTDOWN_INTERVAL) {
+    def onTick(millisUntilFinished: Long): Unit = {
+      val progress: Int = (TIME_TO_WAIT - millisUntilFinished / 100).toInt
+      updateProgressBar(progress)
+    }
+
+    def onFinish(): Unit = {
+      fetchSsoToken()
+    }
+  }
+
+
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View =
     inflater.inflate(R.layout.fragment_start_sso, container, false)
 
-  override def onStart() = {
-    super.onStart()
-    fetchSsoToken()
+  override def onViewCreated(view: View, savedInstanceState: Bundle) = {
+    super.onViewCreated(view, savedInstanceState)
+    showCustomBackendLink()
+    timer.start()
   }
 
   private def fetchSsoToken(): Unit =
@@ -50,6 +71,12 @@ class StartSSOFragment extends SSOFragment {
         }
       }(Threading.Ui)
     }
+
+  private def updateProgressBar(progress: Int): Unit = progressBar.foreach(_.setProgress(progress))
+
+  private def showCustomBackendLink(): Unit = linkTextView.foreach(
+    _.setText(backendController.customBackendConfigUrl.getOrElse(""))
+  )
 }
 
 object StartSSOFragment {
