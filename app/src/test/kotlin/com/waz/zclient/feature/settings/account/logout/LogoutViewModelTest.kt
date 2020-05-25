848eff61ef23da8fd022586c169ebc11e10496e1
Gizem Burhanoglu <gizembrh@gmail.com>
2020-05-25 14:09:47 +0200

    Fix flow.collect and thread change problems

--- app/src/test/kotlin/com/waz/zclient/feature/settings/account/logout/LogoutViewModelTest.kt
+++ app/src/test/kotlin/com/waz/zclient/feature/settings/account/logout/LogoutViewModelTest.kt
@@ -5,7 +5,7 @@ import com.waz.zclient.core.functional.Either
 import com.waz.zclient.eq
 import com.waz.zclient.framework.coroutines.CoroutinesTestRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -30,14 +30,15 @@ class LogoutViewModelTest : UnitTest() {
     }
 
     @Test
-    fun `given a logoutUseCase, when onVerifyButtonClicked is called, calls logoutUseCase`() =
-        runBlockingTest {
+    fun `given a logoutUseCase, when onVerifyButtonClicked is called, calls logoutUseCase`() {
+        runBlocking {
             `when`(logoutUseCase.run(Unit)).thenReturn(Either.Right(NoAccountsLeft))
 
             logoutViewModel.onVerifyButtonClicked()
 
             verify(logoutUseCase).run(eq(Unit))
         }
+    }
 
     @Test
     fun `given logoutUseCase returns success, when onVerifyButtonClicked is called, notifies logoutLiveData`() {
