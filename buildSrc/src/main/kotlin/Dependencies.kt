504220c8b8c940029589cc661de2acf1a515c8cb
Mejdeddine Benzarti <43655743+mejdoo@users.noreply.github.com>
2020-05-26 14:30:36 +0200

    bump version (#2855)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     //wire android client
-    const val ANDROID_CLIENT_MAJOR_VERSION = "3.48."
+    const val ANDROID_CLIENT_MAJOR_VERSION = "3.49."
 
     //core
     const val KOTLIN = "1.3.60"
