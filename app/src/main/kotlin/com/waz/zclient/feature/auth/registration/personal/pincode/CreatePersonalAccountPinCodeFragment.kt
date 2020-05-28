2dd5d2cae00640bc3e99a22b2be34e395a1a145e
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-05-28 18:37:59 +0200

    fix detekt warning

--- app/src/main/kotlin/com/waz/zclient/feature/auth/registration/personal/pincode/CreatePersonalAccountPinCodeFragment.kt
+++ app/src/main/kotlin/com/waz/zclient/feature/auth/registration/personal/pincode/CreatePersonalAccountPinCodeFragment.kt
@@ -1,6 +1,5 @@
 package com.waz.zclient.feature.auth.registration.personal.pincode
 
-import android.content.DialogInterface
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.View
