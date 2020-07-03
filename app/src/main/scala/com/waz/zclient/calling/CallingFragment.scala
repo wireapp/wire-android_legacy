90f8220df3be14362f0b1a35ba06e06d79fa7ede
Mejdeddine Benzarti <43655743+mejdoo@users.noreply.github.com>
2020-07-03 15:45:27 +0200

    [Feature] Conference Calls : VideoGrid  (#2910)
    Customize videoGrid for more than 4 participants

--- app/src/main/scala/com/waz/zclient/calling/CallingFragment.scala
+++ app/src/main/scala/com/waz/zclient/calling/CallingFragment.scala
@@ -201,9 +201,13 @@ class CallingFragment extends FragmentHelper {
             case 0                                             => (0, 0, 1)
             case 1 if !isVideoBeingSent && gridViews.size == 2 => (1, 0, 2)
             case 1                                             => (0, 1, 1)
-            case 2 if gridViews.size == 3                      => (1, 0, 2)
-            case 2                                             => (1, 0, 1)
-            case 3                                             => (1, 1, 1)
+            // The max number of columns is 2 and the max number of rows is undefined
+            // if the index of the video preview is odd, display it in row n/2, column 1 , span 1
+            case n if n % 2 != 0                               => (n / 2, 1, 1)
+            // else if the gridViews size is n+1 , display it in row n/2, column 0 , span 2
+            case n if gridViews.size == n + 1                  => (n / 2, 0, 2)
+            // else display it in row n/2, column 0 , span 1
+            case n                                             => (n / 2, 0, 1)
           }
           r.setLayoutParams(returning(new GridLayout.LayoutParams()) { params =>
             params.width      = 0
