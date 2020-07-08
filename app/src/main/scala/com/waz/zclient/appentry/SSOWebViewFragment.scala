b8f873f0de604e1a0fa60c29e184a5335202266e
Mejdeddine Benzarti <43655743+mejdoo@users.noreply.github.com>
2020-07-08 12:01:17 +0200

    Fix : Wrong welcome screen shown while being connected to a custom backend (#2920)
    * fix : redirect the user to the right welcome screen on back from the OKTA webview

--- app/src/main/scala/com/waz/zclient/appentry/SSOWebViewFragment.scala
+++ app/src/main/scala/com/waz/zclient/appentry/SSOWebViewFragment.scala
@@ -27,21 +27,22 @@ import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentManager
 import com.waz.service.{AccountsService, ZMessaging}
 import com.waz.threading.Threading
+import com.waz.threading.Threading._
 import com.waz.utils._
 import com.waz.utils.wrappers.URI
 import com.waz.zclient.appentry.DialogErrorMessage.EmailError
 import com.waz.zclient.appentry.SSOWebViewWrapper.SSOResponse
 import com.waz.zclient.appentry.fragments.FirstLaunchAfterLoginFragment
 import com.waz.zclient.common.controllers.UserAccountsController
-import com.waz.zclient.utils.{ContextUtils, ViewUtils}
+import com.waz.zclient.utils.{BackendController, ContextUtils, ViewUtils}
 import com.waz.zclient.{FragmentHelper, R}
 
 import scala.concurrent.Future
-import com.waz.threading.Threading._
 
 class SSOWebViewFragment extends FragmentHelper {
   import Threading.Implicits.Ui
 
+  private lazy val backendController = inject[BackendController]
   private lazy val webView = view[WebView](R.id.web_view)
 
   override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
@@ -87,7 +88,7 @@ class SSOWebViewFragment extends FragmentHelper {
   }
 
   override def onBackPressed(): Boolean = {
-    getFragmentManager.popBackStack(SSOWebViewFragment.Tag, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    showWelcomeScreen()
     inject[UserAccountsController].ssoToken ! None
     true
   }
@@ -122,8 +123,16 @@ class SSOWebViewFragment extends FragmentHelper {
     }
   }
 
-  def activity = getActivity.asInstanceOf[AppEntryActivity]
+  private def showWelcomeScreen(): Unit = {
+    activity.getSupportFragmentManager.popBackStackImmediate(SSOWebViewFragment.Tag, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    if (backendController.hasCustomBackend) {
+      activity.showFragment(new CustomBackendLoginFragment, CustomBackendLoginFragment.TAG, animated = false)
+    } else {
+      activity.showFragment(WelcomeFragment(), WelcomeFragment.Tag, animated = false)
+    }
+  }
 
+  def activity = getActivity.asInstanceOf[AppEntryActivity]
 }
 
 object SSOWebViewFragment {
