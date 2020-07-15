c0f06f705d8108b4b12f1a812706fe8edee77487
Maciej Gorywoda <makingthematrix@gmail.com>
2020-07-15 14:36:39 +0200

    Backup functionality: Adding local data sources to DI

--- app/src/main/kotlin/com/waz/zclient/shared/backup/di/BackupModule.kt
+++ app/src/main/kotlin/com/waz/zclient/shared/backup/di/BackupModule.kt
@@ -2,9 +2,23 @@ package com.waz.zclient.shared.backup.di
 
 import com.waz.zclient.shared.backup.BackupRepository
 import com.waz.zclient.shared.backup.datasources.BackupDataSource
+import com.waz.zclient.shared.backup.datasources.local.*
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val backupModule: Module = module {
     single { BackupDataSource(get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get(), get()) as BackupRepository }
+    factory { AssetLocalDataSource(get()) }
+    factory { ButtonLocalDataSource(get()) }
+    factory { ConversationFoldersLocalDataSource(get()) }
+    factory { ConversationMembersLocalDataSource(get()) }
+    factory { ConversationRoleActionLocalDataSource(get()) }
+    factory { ConversationsLocalDataSource(get()) }
+    factory { FoldersLocalDataSource(get()) }
+    factory { KeyValuesLocalDataSource(get()) }
+    factory { LikesLocalDataSource(get()) }
+    factory { MessagesLocalDataSource(get()) }
+    factory { PropertiesLocalDataSource(get()) }
+    factory { ReadReceiptsLocalDataSource(get()) }
+    factory { UserLocalDataSource(get()) }
 }
