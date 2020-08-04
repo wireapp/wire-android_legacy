f29b5401cb3c344cce86463b4f30c1dccec1b58e
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-08-04 15:32:06 +0100

    fix: ignore case while sorting participants in group calls

--- app/src/main/scala/com/waz/zclient/calling/controllers/CallController.scala
+++ app/src/main/scala/com/waz/zclient/calling/controllers/CallController.scala
@@ -125,7 +125,7 @@ class CallController(implicit inj: Injector, cxt: WireContext, eventContext: Eve
   }
 
   def orderedParticipantInfos(take: Option[Int] = None): Signal[Vector[CallParticipantInfo]] =
-    participantInfos(take).map(_.sortBy(_.displayName))
+    participantInfos(take).map(_.sortBy(_.displayName.toLowerCase))
 
   def participantInfos(take: Option[Int] = None): Signal[Vector[CallParticipantInfo]] =
     for {
