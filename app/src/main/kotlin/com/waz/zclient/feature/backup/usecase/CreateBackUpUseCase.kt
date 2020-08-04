2fb30d01c6d16f449555c30bde727d09b2fcfea6
Bradley Wilson <bradley.wilson@wire.com>
2020-08-04 14:37:31 +0200

    create back up use case tests

--- app/src/main/kotlin/com/waz/zclient/feature/backup/usecase/CreateBackUpUseCase.kt
+++ app/src/main/kotlin/com/waz/zclient/feature/backup/usecase/CreateBackUpUseCase.kt
@@ -5,22 +5,24 @@ import com.waz.zclient.core.exception.FeatureFailure
 import com.waz.zclient.core.functional.Either
 import com.waz.zclient.core.usecase.UseCase
 import com.waz.zclient.feature.backup.BackUpRepository
+import kotlinx.coroutines.*
 
-class CreateBackUpUseCase(private val backUpRepositories: List<BackUpRepository>) : UseCase<Unit, Unit> {
+class CreateBackUpUseCase(
+    private val backUpRepositories: List<BackUpRepository>,
+    private val coroutineScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
+) : UseCase<Unit, Unit> {
 
     override suspend fun run(params: Unit): Either<Failure, Unit> {
-        var failure = false
-        //TODO we can speed it up by async + await maybe?
-        for (repo in backUpRepositories) {
-            if (repo.backUp().isLeft) {
-                failure = true
-                break
-            }
-        }
         //TODO would be nice to log the actual exception somewhere
         //TODO rollback changes if something goes wrong
-        return if (failure) Either.Left(BackUpCreationFailure) else Either.Right(Unit)
+        return if (hasBackupFailed()) Either.Left(BackUpCreationFailure) else Either.Right(Unit)
     }
+
+    private suspend fun hasBackupFailed(): Boolean =
+        backUpRepositories
+            .map { coroutineScope.async(Dispatchers.IO) { it.backUp() } }
+            .awaitAll()
+            .any { it.isLeft }
 }
 
 object BackUpCreationFailure : FeatureFailure()
