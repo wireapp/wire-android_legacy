7c2ac59370af1da7e45d27976dae7101b181b3af
Maciej Gorywoda <makingthematrix@gmail.com>
2020-08-07 14:06:11 +0200

    changes to unit tests requested in the code review

--- app/src/test/kotlin/com/waz/zclient/feature/backup/io/file/BackUpFileIOHandlerTest.kt
+++ app/src/test/kotlin/com/waz/zclient/feature/backup/io/file/BackUpFileIOHandlerTest.kt
@@ -1,6 +1,7 @@
 package com.waz.zclient.feature.backup.io.file
 
 import com.waz.zclient.UnitTest
+import com.waz.zclient.core.functional.Either
 import com.waz.zclient.core.utilities.converters.JsonConverter
 import com.waz.zclient.feature.backup.assertItems
 import com.waz.zclient.feature.backup.io.BatchReader
@@ -11,6 +12,7 @@ import org.junit.Assert.fail
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
+import scala.util.Right
 import java.io.File
 
 class BackUpFileIOHandlerTest : UnitTest() {
@@ -24,7 +26,7 @@ class BackUpFileIOHandlerTest : UnitTest() {
     private lateinit var backUpFileIOHandler: BackUpFileIOHandler<Int>
 
     @Test
-    fun `given an iterator, when write() is called, then create a new file and writes contents to it`() =
+    fun `given an iterator, when write() is called, then create a new file and write contents to it`() =
         runTestWithFile(uniqueFileName()) { name ->
             runBlocking {
                 val jsonStr = """
@@ -41,11 +43,45 @@ class BackUpFileIOHandlerTest : UnitTest() {
                 val tempDir = createTempDir()
                 backUpFileIOHandler = BackUpFileIOHandler(name, jsonConverter, tempDir)
                 backUpFileIOHandler.write(batchReader)
-                val createdFiles = getFilesWithPrefix(tempDir, name)
-                assertEquals(1, createdFiles.size)
-                with(createdFiles[0]) {
-                    val text = this.readText()
-                    assertEquals(jsonStr, text)
+                getFilesWithPrefix(tempDir, name).also {
+                    assertEquals(1, it.size)
+                    assertEquals(jsonStr, it.first().readText())
+                }
+            }
+        }
+
+    @Test
+    fun `given an iterator, when write() is called, then create two files and write contents to them`() =
+        runTestWithFile(uniqueFileName()) { name ->
+            runBlocking {
+                val jsonStr1 = """
+                    [
+                        "line 1",
+                        "line 2",
+                        "line 3"
+                    ]
+                """.trimIndent()
+                val jsonStr2 = """
+                    [
+                        "line 4",
+                        "line 5",
+                        "line 6"
+                    ]
+                """.trimIndent()
+
+                batchReader.mockNextItems(listOf(listOf(1, 2, 3), listOf(4, 5, 6)))
+                `when`(jsonConverter.toJsonList(listOf(1, 2, 3))).thenReturn(jsonStr1)
+                `when`(jsonConverter.toJsonList(listOf(4, 5, 6))).thenReturn(jsonStr2)
+
+                val tempDir = createTempDir()
+                backUpFileIOHandler = BackUpFileIOHandler(name, jsonConverter, tempDir)
+                when (val res = backUpFileIOHandler.write(batchReader)) {
+                    is Either.Right -> res.b.let {
+                        assertEquals(2, it.size)
+                        assertEquals(jsonStr1, it[0].readText())
+                        assertEquals(jsonStr2, it[1].readText())
+                    }
+                    is Either.Left -> fail(res.a.toString())
                 }
             }
         }
@@ -78,20 +114,18 @@ class BackUpFileIOHandlerTest : UnitTest() {
 
                 batchReader.mockNextItems(listOf(listOf(1, 2, 3)))
                 backUpFileIOHandler.write(batchReader)
-                val createdFiles1 = getFilesWithPrefix(tempDir, name)
-                assertEquals(1, createdFiles1.size)
-                val fileName = createdFiles1[0].name
+                val firstBatch = getFilesWithPrefix(tempDir, name).also {
+                    assertEquals(1, it.size)
+                    assertEquals(jsonStr1, it.first().readText())
+                }
 
                 batchReader.mockNextItems(listOf(listOf(4, 5, 6)))
 
                 backUpFileIOHandler.write(batchReader)
-                val createdFiles2 = getFilesWithPrefix(tempDir, name)
-                assertEquals(1, createdFiles2.size)
-                assertEquals(fileName, createdFiles2[0].name)
-
-                with(createdFiles2[0]) {
-                    val text = this.readText()
-                    assertEquals(jsonStr2, text)
+                getFilesWithPrefix(tempDir, name).also {
+                    assertEquals(1, it.size)
+                    assertEquals(firstBatch.first().name, it.first().name)
+                    assertEquals(jsonStr2, it.first().readText())
                 }
             }
         }
@@ -124,9 +158,10 @@ class BackUpFileIOHandlerTest : UnitTest() {
         }
 
     companion object {
-        private fun uniqueFileName() = "backUpFileIOHandlerTest_${System.currentTimeMillis()}.txt"
+        private fun uniqueFileName() = "backUpFileIOHandlerTest_${System.currentTimeMillis()}"
 
-        private fun getFilesWithPrefix(dir: File, prefix: String) = dir.walkTopDown().toList().filter { it.name.startsWith(prefix) }
+        private fun getFilesWithPrefix(dir: File, prefix: String) =
+            dir.walkTopDown().toList().filter { it.name.startsWith(prefix) }.sortedBy { it.path }
 
         private fun runTestWithFile(fileName: String, testWithFile: (String) -> Unit): Unit {
             try {
