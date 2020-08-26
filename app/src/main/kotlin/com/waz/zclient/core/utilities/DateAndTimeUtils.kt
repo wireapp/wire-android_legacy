8f638e83adefb7b9344a0ef6ac3359c4af71e9a6
Maciej Gorywoda <makingthematrix@gmail.com>
2020-08-26 13:28:35 +0200

    New backups: A utility method for converting time to strings according to the given format
    Required as an additional change in one of previous code reviews.

--- app/src/main/kotlin/com/waz/zclient/core/utilities/DateAndTimeUtils.kt
+++ app/src/main/kotlin/com/waz/zclient/core/utilities/DateAndTimeUtils.kt
@@ -4,26 +4,40 @@ import org.threeten.bp.Duration
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-//Utils class - do not instantiate
-class DateAndTimeUtils private constructor() {
+enum class DatePattern(val pattern: String) {
+    DATE("yyyy-MM-dd"),
+    DATE_SHORT("yyyyMMdd"),
+    DATE_TIME("yyyy-MM-dd HH:mm:ss"),
+    DATE_TIME_SHORT("yyyyMMddHHmmss"),
+    DATE_TIME_ZONE("yyyy-MM-dd HH:mm:ss'Z'"),
+    DATE_TIME_LONG("yyyy-MM-dd HH:mm:ss.SSS"),
+    DATE_TIME_ZONE_LONG("yyyy-MM-dd HH:mm:ss.SSS'Z'")
+}
 
-    companion object {
+object DateAndTimeUtils {
+    private const val TWO_MINUTES = 2L
+    private const val SIXTY_MINUTES = 60L
 
-        private const val TWO_MINUTES = 2L
-        private const val SIXTY_MINUTES = 60L
+    fun getTimeStamp(time: String): String {
+        val now = LocalDateTime.now()
+        val localTime = LocalDateTime.ofInstant(Instant.parse(time), ZoneId.systemDefault())
+        val isLastTwoMins = now.minusMinutes(TWO_MINUTES).isBefore(localTime)
+        val isLastSixtyMinutes = now.minusMinutes(SIXTY_MINUTES).isBefore(localTime)
 
-        fun getTimeStamp(time: String): String {
-            val now = LocalDateTime.now()
-            val localTime = LocalDateTime.ofInstant(Instant.parse(time), ZoneId.systemDefault())
-            val isLastTwoMins = now.minusMinutes(TWO_MINUTES).isBefore(localTime)
-            val isLastSixtyMinutes = now.minusMinutes(SIXTY_MINUTES).isBefore(localTime)
-
-            return when {
-                isLastTwoMins -> "Just now"
-                isLastSixtyMinutes -> "${Duration.between(localTime, now).toMinutes().toInt()} minutes ago"
-                else -> time
-            }
+        return when {
+            isLastTwoMins -> "Just now"
+            isLastSixtyMinutes -> "${Duration.between(localTime, now).toMinutes().toInt()} minutes ago"
+            else -> time
         }
     }
+
+    fun instantToString(
+        instant: Instant = Instant.now(),
+        pattern: DatePattern = DatePattern.DATE_TIME,
+        locale: Locale = Locale.getDefault()
+    ): String =
+        SimpleDateFormat(pattern.pattern, locale).format(instant.toEpochMilli())
 }
