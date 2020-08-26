NgCongMinh-Dev/wire-android

New Backups: Create a backup from UI
This PR contains code connecting Scala and Kotlin (1) and of a fix to BackUpDataSource<T, E> (2)

1.
In BackupExportView in Scala I switched from calling the old backup functionality in BackupManager to calling the new functionality through
KotlinServices and BackUpViewModel. This is by necessity a bit hacky: KotlinServices provides a way to call Kotlin from Scala, but it's not
a fragment or an activity, so injecting BackUpViewModel is done through a small additional Koin library: koin-java. Its official purpose is to
enable calling Koin from Java, but it turns out it also greatly simplifies calling Koin from Kotlin where standard methods don't work.

BackUpViewModel handles CreateBackUpUseCase and in the future it should also handle RestoreBackUpUseCase. It hides from KotlinServices (and Scala)
the fact that we use coroutines. Instead, Scala provides two callbacks: onSuccess and onFailure. One (and only one) of them should be called when
the Kotlin side finishes working.

2.
The bug was that the abstract type BackUpRepository<List<File>> was referred to and treated as BackUpRepository<File> in CreateBackUpUseCase.
The compiler didn't catch it, and on top of that Koin was happy to inject the repositories even though their types didn't fit. Only when I started
to test manually, the app crashed with a ClassCastException.
Apart from fixing the type, I had to add names to arguments in Koin. It was necessary also for the point #1 to let Koin know how to create
BackUpViewModel.

I made also some changes to the how we write json files and then zip them, but this part will be changed once again. Here I just wanted to make sure
it works.

app/src/main/kotlin/com/waz/zclient/feature/backup/usecase/CreateBackUpUseCase.kt
@@ -22,26 +22,29 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 class CreateBackUpUseCase(
-    private val backUpRepositories: List<BackUpRepository<File>>,
+    private val backUpRepositories: List<BackUpRepository<List<File>>>,
     private val zipHandler: ZipHandler,
     private val encryptionHandler: EncryptionHandler,
     private val metaDataHandler: MetaDataHandler,
     private val coroutineScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
-) : UseCase<File, Triple<UserId, String, String>> {
+) : UseCase<File, CreateBackUpUseCaseParams> {
 
-    override suspend fun run(params: Triple<UserId, String, String>): Either<Failure, File> {
-        val userId = params.first
-        val userHandle = params.second
-        val password = params.third
+    override suspend fun run(params: CreateBackUpUseCaseParams): Either<Failure, File> =
+        backUpOrFail()
+            .flatMap { files ->
+                metaDataHandler.generateMetaDataFile(params.userId, params.userHandle).map { files + it }
+            }
+            .flatMap { files ->
+                zipHandler.zip(backupZipFileName(params.userHandle), files)
+            }
+            .flatMap { file ->
+                encryptionHandler.encrypt(file, params.userId, params.password)
+            }
 
-        return backUpOrFail()
-            .flatMap { files -> metaDataHandler.generateMetaDataFile(userId, userHandle).map { files + it } }
-            .flatMap { zipHandler.zip(backupZipFileName(userHandle), it) }
-            .flatMap { encryptionHandler.encrypt(it, userId, password) }
-    }
-
-    private suspend fun backUpOrFail() = extractFiles(
-        backUpRepositories.map { coroutineScope.async(Dispatchers.IO) { it.saveBackup() } }.awaitAll()
+    private suspend fun backUpOrFail(): Either<Failure, List<File>> = extractFiles(
+        backUpRepositories
+            .map { coroutineScope.async(Dispatchers.IO) { it.saveBackup() } }
+            .awaitAll()
     )
 
     @SuppressWarnings("MagicNumber")
@@ -50,19 +53,18 @@ class CreateBackUpUseCase(
         return "Wire-$userHandle-Backup_$timestamp.android_wbu"
     }
 
-    private fun extractFiles(list: List<Either<Failure, File>>): Either<Failure, List<File>> {
+    private fun extractFiles(list: List<Either<Failure, List<File>>>): Either<Failure, List<File>> {
         val files = mutableListOf<File>()
 
-        for (value in list)
-            when (value) {
-                is Either.Right -> files += value.b
-                is Either.Left -> return Either.Left(value.a)
+        list.forEach {
+            when (it) {
+                is Either.Right -> files.addAll(it.b)
+                is Either.Left -> return Either.Left(it.a)
             }
+        }
 
         return Either.Right(files.toList())
     }
-
-    companion object {
-        const val TAG = "CreateBackUpUseCase"
-    }
 }
+
+data class CreateBackUpUseCaseParams(val userId: UserId, val userHandle: String, val password: String)

wireapp/wire-android_legacy

app/src/main/kotlin/com/waz/zclient/feature/backup/usecase/CreateBackUpUseCase.kt
@@ -22,26 +22,29 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 class CreateBackUpUseCase(
-    private val backUpRepositories: List<BackUpRepository<File>>,
+    private val backUpRepositories: List<BackUpRepository<List<File>>>,
     private val zipHandler: ZipHandler,
     private val encryptionHandler: EncryptionHandler,
     private val metaDataHandler: MetaDataHandler,
     private val coroutineScope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default)
-) : UseCase<File, Triple<UserId, String, String>> {
+) : UseCase<File, CreateBackUpUseCaseParams> {
 
-    override suspend fun run(params: Triple<UserId, String, String>): Either<Failure, File> {
-        val userId = params.first
-        val userHandle = params.second
-        val password = params.third
+    override suspend fun run(params: CreateBackUpUseCaseParams): Either<Failure, File> =
+        backUpOrFail()
+            .flatMap { files ->
+                metaDataHandler.generateMetaDataFile(params.userId, params.userHandle).map { files + it }
+            }
+            .flatMap { files ->
+                zipHandler.zip(backupZipFileName(params.userHandle), files)
+            }
+            .flatMap { file ->
+                encryptionHandler.encrypt(file, params.userId, params.password)
+            }
 
-        return backUpOrFail()
-            .flatMap { files -> metaDataHandler.generateMetaDataFile(userId, userHandle).map { files + it } }
-            .flatMap { zipHandler.zip(backupZipFileName(userHandle), it) }
-            .flatMap { encryptionHandler.encrypt(it, userId, password) }
-    }
-
-    private suspend fun backUpOrFail() = extractFiles(
-        backUpRepositories.map { coroutineScope.async(Dispatchers.IO) { it.saveBackup() } }.awaitAll()
+    private suspend fun backUpOrFail(): Either<Failure, List<File>> = extractFiles(
+        backUpRepositories
+            .map { coroutineScope.async(Dispatchers.IO) { it.saveBackup() } }
+            .awaitAll()
     )
 
     @SuppressWarnings("MagicNumber")
@@ -50,19 +53,18 @@ class CreateBackUpUseCase(
         return "Wire-$userHandle-Backup_$timestamp.android_wbu"
     }
 
-    private fun extractFiles(list: List<Either<Failure, File>>): Either<Failure, List<File>> {
+    private fun extractFiles(filesOrFailure: List<Either<Failure, List<File>>>): Either<Failure, List<File>> {
         val files = mutableListOf<File>()
 
-        for (value in list)
-            when (value) {
-                is Either.Right -> files += value.b
-                is Either.Left -> return Either.Left(value.a)
+        filesOrFailure.forEach {
+            when (it) {
+                is Either.Right -> files.addAll(it.b)
+                is Either.Left -> return Either.Left(it.a)
             }
+        }
 
         return Either.Right(files.toList())
     }
-
-    companion object {
-        const val TAG = "CreateBackUpUseCase"
-    }
 }
+
+data class CreateBackUpUseCaseParams(val userId: UserId, val userHandle: String, val password: String)