3fc1591550401243baaaf97e97fef7ca18a63b03
Maciej Gorywoda <makingthematrix@gmail.com>
2020-10-05 11:00:22 +0200

    Version bump to 3.57

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     //wire android client
-    const val ANDROID_CLIENT_MAJOR_VERSION = "3.56."
+    const val ANDROID_CLIENT_MAJOR_VERSION = "3.57."
 
     //core
     const val KOTLIN = "1.3.72"
