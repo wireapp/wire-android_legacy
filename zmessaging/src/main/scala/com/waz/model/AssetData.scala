07a0a301a9895894132c6d7f59681437a30692b6
Maciej Gorywoda <makingthematrix@gmail.com>
2020-11-03 17:30:07 +0100

    fix: Get random profile pictures from Unsplash (#3081)
    * fix: Get random profile pictures from Unsplash
    
    fixes https://wearezeta.atlassian.net/browse/AN-7047
    
    I'm not exactly sure how long this functionality was broken but it looks like it was for a long time. The previous code
    tried to treat an Unsplash URL as if it was a regular asset that was supposed to be downloaded through our backend.
    The backend failed because it didn't understand the request.I rewrote `AccountManager.addUnsplashPicture()` and added methods to `AssetService` and `AssetClient` which download the picture from Unsplash. Then the picture is set as the profile one and sent to the backend exactly as if it was a regular profile picture.
    
    I also added a button in Dev Settings to test it. From now on it can be used if we want to test any change in code connected to profile pictures.
    
    * changes requested in the review

--- zmessaging/src/main/scala/com/waz/model/AssetData.scala
+++ zmessaging/src/main/scala/com/waz/model/AssetData.scala
@@ -25,7 +25,8 @@ import com.waz.model.AssetMetaData.Image.Tag
 import com.waz.model.AssetStatus.{UploadCancelled, UploadDone}
 import com.waz.model.GenericContent.EncryptionAlgorithm
 import com.waz.model.otr.SignalingKey
-import com.waz.service.{UserService, ZMessaging}
+import com.waz.service.ZMessaging
+import com.waz.sync.client.AssetClient
 import com.waz.utils.JsonDecoder.{apply => _, opt => _}
 import com.waz.utils._
 import com.waz.utils.crypto.AESUtils
@@ -72,16 +73,12 @@ case class AssetData(override val id: AssetId               = AssetId(),
     case _ => Some(RemoteData(remoteId, token, otrKey, sha, encryption))
   }
 
-  lazy val cacheKey = {
-    val key = (proxyPath, source) match {
-      case (Some(proxy), _)                            => CacheKey(proxy)
-      case (_, Some(uri)) if !NonKeyURIs.contains(uri) => CacheKey.fromUri(uri)
-      case _                                           => CacheKey.fromAssetId(id)
-    }
-    //verbose(s"created cache key: $key for asset: $id")
-    key
+  lazy val cacheKey: CacheKey = (proxyPath, source) match {
+    case (Some(proxy), _)                                          => CacheKey(proxy)
+    case (_, Some(uri)) if uri.getPath != AssetClient.UnsplashPath => CacheKey.fromUri(uri)
+    case _                                                         => CacheKey.fromAssetId(id)
   }
-
+  
   lazy val isDownloadable = this match {
     case WithRemoteData(_)  => true
     case WithExternalUri(_) => true
@@ -121,11 +118,6 @@ case class AssetData(override val id: AssetId               = AssetId(),
 }
 
 object AssetData {
-
-  /**
-    * Do not use these URIs as cache keys, as they do not provide a unique identifier to the asset downloaded from them
-    */
-  val NonKeyURIs: Set[URI] = Set(UserService.UnsplashUrl)
 
   def decodeData(data64: String): Array[Byte] = AESUtils.base64(data64)
 
