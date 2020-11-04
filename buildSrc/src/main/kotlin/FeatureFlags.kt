d05e01017ab00c449c75fc3214cad7c3963622d0
Mejdeddine Benzarti <mejdeddine.benzarti@wire.com>
2020-11-04 15:38:14 +0100

    fix styling issues

--- buildSrc/src/main/kotlin/FeatureFlags.kt
+++ buildSrc/src/main/kotlin/FeatureFlags.kt
@@ -1,6 +1,6 @@
 // Kotlin Feature Flags
 object FeatureFlags {
-    const val REGITRATION = false
+    const val REGISTRATION = false
     const val SETTINGS = false
-    val CORE = REGITRATION || SETTINGS
+    val CORE = REGISTRATION || SETTINGS
 }
