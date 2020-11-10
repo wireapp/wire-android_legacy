7caa370c29091840b623dca851a27df3e00d29f1
Mejdeddine Benzarti <mejdeddine.benzarti@wire.com>
2020-11-10 18:05:40 +0100

    bump client version to 3.59

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -5,7 +5,7 @@ import org.gradle.api.JavaVersion
 
 object Versions {
     //wire android client
-    const val ANDROID_CLIENT_MAJOR_VERSION = "3.58."
+    const val ANDROID_CLIENT_MAJOR_VERSION = "3.59."
     const val COMPILE_SDK_VERSION = 29
     const val TARGET_SDK_VERSION = 29
     const val MIN_SDK_VERSION = 24
