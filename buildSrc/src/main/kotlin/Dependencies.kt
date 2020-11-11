4b4c713da30136714f07e9a33e091b28b4dac504
Mejdeddine Benzarti <mejdeddine.benzarti@wire.com>
2020-11-11 11:48:16 +0100

    bump avs private version to 6.4.19

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -18,7 +18,7 @@ object Versions {
 
     //Wire Libs
     const val AVS_PUBLIC = "6.4.233"
-    const val AVS_PRIVATE = "6.4.16"
+    const val AVS_PRIVATE = "6.4.19"
     const val WIRE_TRANSLATIONS = "1.+"
     val WIRE_AUDIO = System.getenv("AUDIO_VERSION") ?: "1.209.0@aar"
 
