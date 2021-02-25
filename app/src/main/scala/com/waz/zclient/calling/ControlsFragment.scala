4abe3a3aa52cd349b35ac79ad1db271c2034e774
Mejdeddine Benzarti <mejdeddine@wire.com>
2021-02-25 18:18:02 +0100

    chore : enable speakers/all toggle

--- app/src/main/scala/com/waz/zclient/calling/ControlsFragment.scala
+++ app/src/main/scala/com/waz/zclient/calling/ControlsFragment.scala
@@ -31,8 +31,8 @@ import com.waz.zclient.calling.views.{CallingHeader, CallingMiddleLayout, Contro
 import com.waz.zclient.log.LogUI._
 import com.waz.zclient.utils.ContextUtils._
 import com.waz.zclient.utils.{RichView, ViewUtils}
-import com.waz.zclient.{FragmentHelper, MainActivity, R}
-import com.wire.signals.{Subscription}
+import com.waz.zclient.{BuildConfig, FragmentHelper, MainActivity, R}
+import com.wire.signals.{Signal, Subscription}
 
 class ControlsFragment extends FragmentHelper {
 
@@ -93,9 +93,7 @@ class ControlsFragment extends FragmentHelper {
       }
     }
 
-    //TODO : The calling squad decided to disable all/speaker toggle to perform some optimizations
-    // in terms of user experience before releasing it to public
-    /*if (BuildConfig.ACTIVE_SPEAKERS) {
+    if (BuildConfig.ACTIVE_SPEAKERS) {
        Signal.zip(
          controller.isCallEstablished,
          controller.isGroupCall,
@@ -106,8 +104,7 @@ class ControlsFragment extends FragmentHelper {
          case _                         => speakersLayoutContainer.foreach(_.setVisibility(View.INVISIBLE))
        }
      }
-     else */
-    speakersLayoutContainer.foreach(_.setVisibility(View.INVISIBLE))
+     else speakersLayoutContainer.foreach(_.setVisibility(View.INVISIBLE))
   }
 
   override def onStart(): Unit = {
