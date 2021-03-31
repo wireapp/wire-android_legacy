92dd0dcb4628061c17b293ba66099989d63f25ab
John Nguyen <polyxo@protonmail.com>
2021-03-31 16:20:53 +0200

    Update service tests

--- zmessaging/src/test/scala/com/waz/service/LegalHoldServiceSpec.scala
+++ zmessaging/src/test/scala/com/waz/service/LegalHoldServiceSpec.scala
@@ -3,9 +3,11 @@ package com.waz.service
 import com.waz.content.{PropertiesStorage, PropertyValue}
 import com.waz.specs.AndroidFreeSpec
 import LegalHoldService._
+import com.waz.api.impl.ErrorResponse
 import com.waz.model.{LegalHoldRequest, LegalHoldRequestEvent}
 import com.waz.model.otr.ClientId
 import com.waz.service.EventScheduler.{Sequential, Stage}
+import com.waz.sync.SyncResult
 import com.waz.sync.handler.LegalHoldSyncHandler
 import com.waz.utils.JsonEncoder
 import com.waz.utils.crypto.AESUtils
@@ -70,12 +72,69 @@ class LegalHoldServiceSpec extends AndroidFreeSpec {
 
       // Then
       (storage.save _)
-        .expects( PropertyValue(LegalHoldRequestKey, encodedLegalHoldRequest))
+        .expects(PropertyValue(LegalHoldRequestKey, encodedLegalHoldRequest))
         .once()
         .returning(Future.successful({}))
 
       // When
       result(pipeline.apply(Seq(event)))
+    }
+  }
+
+  feature("Sync legal hold request") {
+
+    scenario("it succeeds if legal hold request exists") {
+      // Given
+      val service = new LegalHoldServiceImpl(storage, syncHandler)
+
+      (syncHandler.fetchLegalHoldRequest _ )
+        .expects()
+        .once()
+        .returning(Future.successful(Right(Some(legalHoldRequest))))
+
+      (storage.save _)
+        .expects(PropertyValue(LegalHoldRequestKey, encodedLegalHoldRequest))
+        .once()
+        .returning(Future.successful({}))
+
+      // When
+      val actualResult = result(service.syncLegalHoldRequest())
+
+      // Then
+      actualResult shouldEqual SyncResult.Success
+    }
+
+    scenario("it succeeds if legal hold does not exist") {
+      // Given
+      val service = new LegalHoldServiceImpl(storage, syncHandler)
+
+      (syncHandler.fetchLegalHoldRequest _ )
+        .expects()
+        .once()
+        .returning(Future.successful(Right(None)))
+
+      // When
+      val actualResult = result(service.syncLegalHoldRequest())
+
+      // Then
+      actualResult shouldEqual SyncResult.Success
+    }
+
+    scenario("it fails if an error occurs") {
+      // Given
+      val service = new LegalHoldServiceImpl(storage, syncHandler)
+      val error = ErrorResponse(400, "", "")
+
+      (syncHandler.fetchLegalHoldRequest _ )
+        .expects()
+        .once()
+        .returning(Future.successful(Left(error)))
+
+      // When
+      val actualResult = result(service.syncLegalHoldRequest())
+
+      // Then
+      actualResult shouldEqual SyncResult.Failure(error)
     }
   }
 }
