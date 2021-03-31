147992494cbe7f85e46776c6aa03ff7b3dc0e197
John Nguyen <polyxo@protonmail.com>
2021-03-31 12:02:40 +0200

    Add legal hold service

---
+++ zmessaging/src/main/scala/com/waz/service/LegalHoldService.scala
@@ -0,0 +1,42 @@
+package com.waz.service
+
+import com.waz.content.{PropertiesStorage, PropertyValue}
+import com.waz.model.{LegalHoldRequest, LegalHoldRequestEvent}
+import com.waz.service.EventScheduler.Stage
+import com.waz.utils.{JsonDecoder, JsonEncoder}
+
+import scala.concurrent.Future
+
+trait LegalHoldService {
+  def legalHoldRequestEventStage: Stage.Atomic
+  def fetchLegalHoldRequest: Future[Option[LegalHoldRequest]]
+}
+
+class LegalHoldServiceImpl(storage: PropertiesStorage)
+  extends LegalHoldService {
+
+  import com.waz.threading.Threading.Implicits.Background
+  import LegalHoldService._
+
+  override def legalHoldRequestEventStage: Stage.Atomic = EventScheduler.Stage[LegalHoldRequestEvent] { (_, events) =>
+    Future.sequence(events.map(event => storeRequest(event.request))).map(_ => ())
+  }
+
+  override def fetchLegalHoldRequest: Future[Option[LegalHoldRequest]] = {
+    storage.find(LegalHoldRequestKey).map { property =>
+      property.map(_.value).map(JsonDecoder.decode[LegalHoldRequest])
+    }
+  }
+
+  private def storeRequest(request: LegalHoldRequest): Future[Unit] = {
+    val value = JsonEncoder.encode[LegalHoldRequest](request).toString
+    storage.save(PropertyValue(LegalHoldRequestKey, value))
+  }
+
+}
+
+object LegalHoldService {
+
+  val LegalHoldRequestKey: PropertyKey = PropertyKey("legal-hold-request")
+
+}
