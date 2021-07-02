846279c8764cd3ea6a8d892730e186360fbae389
Maciej Gorywoda <makingthematrix@gmail.com>
2021-07-02 16:49:01 +0200

    wire-signals bump to 1.0.0 (#3398)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -189,8 +189,8 @@ object LegacyDependencies {
     const val SCALA_MAJOR_VERSION = "2.11"
     const val SCALA_VERSION = SCALA_MAJOR_VERSION.plus(".12")
     // signals
-    const val WIRE_SIGNALS = "0.5.2"
-    const val WIRE_SIGNALS_EXTENSIONS = "0.5.2"
+    const val WIRE_SIGNALS = "1.0.0"
+    const val WIRE_SIGNALS_EXTENSIONS = "1.0.0"
 
     //build
     val scalaLibrary = "org.scala-lang:scala-library:$SCALA_VERSION"
