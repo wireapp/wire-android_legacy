5a44ffea92495f5d1d66a529c27f93b8409af9b0
vitorhugods <vitor@schwaab.xyz>
2021-08-24 10:38:43 -0300

    Fix wrong enabled status when deserializing `SelfDeletingMessagesFeatureConfig`

--- zmessaging/src/main/scala/com/waz/model/SelfDeletingMessagesFeatureConfig.scala
+++ zmessaging/src/main/scala/com/waz/model/SelfDeletingMessagesFeatureConfig.scala
@@ -16,7 +16,7 @@ object SelfDeletingMessagesFeatureConfig {
   implicit object Decoder extends JsonDecoder[SelfDeletingMessagesFeatureConfig] {
     override def apply(implicit js: JSONObject): SelfDeletingMessagesFeatureConfig =
       if (!js.has("status")) Default
-      else if (js.getString("status") != "enabled") SelfDeletingMessagesFeatureConfig(isEnabled = true, 0)
+      else if (js.getString("status") != "enabled") SelfDeletingMessagesFeatureConfig(isEnabled = false, 0)
       else {
         val config = js.optJSONObject("config")
         val enforcedTimeoutSeconds = if (config != null && config.has("enforcedTimeoutSeconds"))
