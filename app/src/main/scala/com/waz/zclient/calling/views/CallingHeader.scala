57f9696beff6445d11106279c791861eac8f12d4
Maciej Gorywoda <makingthematrix@gmail.com>
2022-02-17 13:04:51 +0100

    feat: Securpty clearance on the call view (FS-385) (#3671)
    https://wearezeta.atlassian.net/browse/FS-385
    
    This PR adds the security clearance banner on the call view, as designed in the JIRA ticket.
    The banner responds to the securpty clearance of participants of the conversation, not those only in the call.
    The banner changes in real time.
    
    The calling functionality is separated from the text-based conversations functionality so I had to copy-paste some of the code.
    I would prefer to leave it like this instead of making some complex connections between classes only to avoid duplication.

--- app/src/main/scala/com/waz/zclient/calling/views/CallingHeader.scala
+++ app/src/main/scala/com/waz/zclient/calling/views/CallingHeader.scala
@@ -20,25 +20,27 @@ package com.waz.zclient.calling.views
 
 import android.content.Context
 import android.util.AttributeSet
-import android.widget.{LinearLayout, TextView}
-import com.waz.content.UserPreferences
+import android.widget.{FrameLayout, LinearLayout, TextView}
 import com.waz.threading.Threading._
 import com.waz.zclient.calling.controllers.CallController
 import com.waz.zclient.common.views.GlyphButton
-import com.waz.zclient.utils.ContextUtils.getString
+import com.waz.zclient.participants.ParticipantsController.ClassifiedConversation
+import com.waz.zclient.ui.text.TypefaceTextView
+import com.waz.zclient.utils.ContextUtils.{getColor, getString}
 import com.waz.zclient.{R, ViewHelper}
 import com.wire.signals.Signal
+import com.waz.zclient.utils._
 
-class CallingHeader(val context: Context, val attrs: AttributeSet, val defStyleAttr: Int) extends LinearLayout(context, attrs, defStyleAttr) with ViewHelper {
+class CallingHeader(val context: Context, val attrs: AttributeSet, val defStyleAttr: Int)
+  extends LinearLayout(context, attrs, defStyleAttr) with ViewHelper {
   def this(context: Context, attrs: AttributeSet) = this(context, attrs, 0)
   def this(context: Context) =  this(context, null)
 
-  private val controller              = inject[CallController]
-  private lazy val vbrSettingsEnabled = inject[Signal[UserPreferences]].flatMap(_.preference(UserPreferences.VBREnabled).signal)
+  private val controller  = inject[CallController]
 
-  private lazy val nameView               = findById[TextView](R.id.ttv__calling__header__name)
-  private lazy val subtitleView           = findById[TextView](R.id.ttv__calling__header__subtitle)
-  private lazy val bitRateModeView        = findById[TextView](R.id.ttv__calling__header__bitrate)
+  private lazy val nameView        = findById[TextView](R.id.ttv__calling__header__name)
+  private lazy val subtitleView    = findById[TextView](R.id.ttv__calling__header__subtitle)
+  private lazy val bitRateModeView = findById[TextView](R.id.ttv__calling__header__bitrate)
 
   lazy val closeButton: GlyphButton = findById[GlyphButton](R.id.calling_header_close)
 
@@ -51,4 +53,30 @@ class CallingHeader(val context: Context, val attrs: AttributeSet, val defStyleA
     case (true, false, Some(true)) => getString(R.string.audio_message_constant_bit_rate)
     case _ => ""
   }.onUi(bitRateModeView.setText)
+
+  private lazy val classifiedBanner = findById[FrameLayout](R.id.call_classified_banner)
+  controller.isCurrentConvClassified.onUi {
+    case ClassifiedConversation.Classified =>
+      classifiedBanner.setBackgroundColor(getColor(R.color.background_light))
+      classifiedBanner.setVisible(true)
+    case ClassifiedConversation.NotClassified =>
+      classifiedBanner.setBackgroundColor(getColor(R.color.background_dark))
+      classifiedBanner.setVisible(true)
+    case ClassifiedConversation.None =>
+      classifiedBanner.setVisible(false)
+  }
+
+  private lazy val classifiedBannerText = findById[TypefaceTextView](R.id.call_classified_banner_text)
+  controller.isCurrentConvClassified.onUi {
+    case ClassifiedConversation.Classified =>
+      classifiedBannerText.setTransformedText(getString(R.string.conversation_is_classified))
+      classifiedBannerText.setTextColor(getColor(R.color.background_dark))
+      classifiedBannerText.setVisible(true)
+    case ClassifiedConversation.NotClassified =>
+      classifiedBannerText.setTransformedText(getString(R.string.conversation_is_not_classified))
+      classifiedBannerText.setTextColor(getColor(R.color.background_light))
+      classifiedBannerText.setVisible(true)
+    case ClassifiedConversation.None =>
+      classifiedBannerText.setVisible(false)
+  }
 }
