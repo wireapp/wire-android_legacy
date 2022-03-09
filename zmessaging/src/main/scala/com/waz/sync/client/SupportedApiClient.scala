7012c5c31ec633bc3ca65715d8fae60a5d13639c
Marco <marcoconti83@gmail.com>
2022-03-09 17:53:41 +0100

    Fix test crash for `NoSuchMethodException`

--- zmessaging/src/main/scala/com/waz/sync/client/SupportedApiClient.scala
+++ zmessaging/src/main/scala/com/waz/sync/client/SupportedApiClient.scala
@@ -19,7 +19,10 @@ final class SupportedApiClientImpl(implicit httpClient: HttpClient)
   import SupportedApiConfig._
 
   override def getSupportedApiVersions(baseUrl: URI): ErrorOrResponse[SupportedApiConfig] = {
-    val appended = baseUrl.buildUpon.appendPath("api-version").build
+    val appended = baseUrl.toString + "api-version"
+    // unfortunately, the nicer code:
+    //      val appended = baseUrl.buildUpon.appendPath("api-version").build
+    // causes a `NoSuchMethodException` when run in tests
     Request.create(Method.Get, new URL(appended.toString))
       .withResultType[SupportedApiConfig]
       .withErrorType[ErrorResponse]
