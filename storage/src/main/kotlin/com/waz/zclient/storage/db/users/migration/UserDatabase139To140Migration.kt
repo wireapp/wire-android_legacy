704d54fd8b22b39aee74f93ecba1df9d645b4754
Marco <marcoconti83@gmail.com>
2022-09-05 10:49:03 +0200

    Do not wipe the decrypted table
    As discussed with Julia, let's start with only wiping the encrypted table as it's safer, and see how that impact performance.

--- storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/UserDatabase139To140Migration.kt
+++ storage/src/main/kotlin/com/waz/zclient/storage/db/users/migration/UserDatabase139To140Migration.kt
@@ -9,13 +9,6 @@ val USER_DATABASE_MIGRATION_139_TO_140 = object : Migration(139, 140) {
         with(database) {
             // Deleting encrypted events is safe: they can be re-fetched and decrypted again
             execSQL("DELETE FROM EncryptedPushNotificationEvents")
-            // Deleting decrypted events is tricky: they can not be decrypted again. Deleting a
-            // message that was not processed yet causes the message to be lost forever.
-            // We can't distinguishing which events are already processed but are still
-            // (wrongfully) in this table. It would be possible for some message types,
-            // but it will be impossible for other. At the cost of losing some encrypted messages, the
-            // easiest solution is to just delete the entire table.
-            execSQL("DELETE FROM DecryptedPushNotificationEvents")
         }
     }
 }
